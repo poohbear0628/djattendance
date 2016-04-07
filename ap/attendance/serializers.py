a47295ef47d7251adb6836514b2f8ac9be85a7d2
[Sam] <[sam.chai0501@gmail.com]>
2016-04-07 15:03:31 -0700

    rest api for attendance
    pip install drf-nested-routers
    pip install django-filter
    pip install django-crispy-forms
    pip install djangorestframework-bulk
    
     - Example url for datetime range.
    http://localhost:8000/api/individualslips/?submitted__gt=2016-03-16+17:00:05&submitted__lt=2016-03-16+17:00:30
    
     - Filter with '?'. http://localhost:8000/api/individualleaveslips/?type=CONF

--- ap/attendance/serializers.py
+++ ap/attendance/serializers.py
@@ -1,15 +1,41 @@
+import django_filters
 from rest_framework.serializers import ModelSerializer
 from .models import Roll
-from rest_framework import serializers
+from rest_framework import serializers, filters
 from accounts.models import Trainee
 from leaveslips.serializers import IndividualSlipSerializer, GroupSlipSerializer
-class RollSerializer(ModelSerializer):
+from schedules.serializers import EventSerializer, ScheduleSerializer
+from rest_framework_bulk import (
+    BulkListSerializer,
+    BulkSerializerMixin,
+    ListBulkCreateUpdateDestroyAPIView,
+)
+
+class RollSerializer(BulkSerializerMixin, ModelSerializer):
+    class Meta(object):
+        model = Roll
+        list_serializer_class = BulkListSerializer
+        fields = '__all__'
+
+class RollFilter(filters.FilterSet):
+    timestamp__lt = django_filters.DateTimeFilter(name = 'timestamp', lookup_expr = 'lt')
+    timestamp__gt = django_filters.DateTimeFilter(name = 'timestamp', lookup_expr = 'gt')
+    finalized = django_filters.BooleanFilter()
     class Meta:
         model = Roll
+        fields = ['id','status','finalized','notes','timestamp','event','trainee','monitor']
 
-class AttendanceSerializer(ModelSerializer):
-    individualslips = IndividualSlipSerializer(many=True, read_only=True)
-    groupslips = GroupSlipSerializer(many=True, read_only=True)
-    class Meta:
+class AttendanceSerializer(BulkSerializerMixin, ModelSerializer):
+    individualslips = IndividualSlipSerializer(many=True,)
+    groupslips = GroupSlipSerializer(many=True,)
+    rolls = RollSerializer(many=True,)
+    schedule = ScheduleSerializer(many=True,)
+    class Meta(object):
    		model = Trainee
+   		list_serializer_class = BulkListSerializer
+   		fields = '__all__'
 
+class AttendanceFilter(filters.FilterSet):
+	class Meta:
+		model = Trainee
+		fields = ['id','individualslips','groupslips','rolls','schedule']
--- ap/attendance/views.py
+++ ap/attendance/views.py
@@ -1,22 +1,22 @@
+import django_filters
 from itertools import chain
-
 from django.views.generic import TemplateView
 from django.core.urlresolvers import reverse_lazy
 from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import get_object_or_404
-
-from rest_framework import viewsets
-
+from rest_framework import viewsets, filters
 from .models import Roll
-from .serializers import RollSerializer, AttendanceSerializer
+from .serializers import RollSerializer, RollFilter, AttendanceSerializer, AttendanceFilter
 from schedules.models import Schedule, Event
 from leaveslips.models import IndividualSlip, GroupSlip
 from terms.models import Term
 from accounts.models import User, Trainee
 from leaveslips.models import IndividualSlip
 from leaveslips.forms import IndividualSlipForm
-
+from rest_framework_bulk import (
+    BulkModelViewSet
+)
 
 class AttendancePersonal(TemplateView):
     template_name = 'attendance/attendance_detail.html'
@@ -24,6 +24,7 @@ class AttendancePersonal(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(AttendancePersonal, self).get_context_data(**kwargs)
+        context['events'] = Event.objects.filter(term=Term.current_term())
         context['trainee'] = self.request.user.trainee
         context['schedule'] = Schedule.objects.filter(term=Term.current_term()).get(trainee=self.request.user.trainee)
         context['attendance'] = Roll.objects.filter(trainee=self.request.user.trainee).filter(event__term=Term.current_term())
@@ -31,16 +32,78 @@ class AttendancePersonal(TemplateView):
         context['leaveslips'] = chain(list(IndividualSlip.objects.filter(trainee=self.request.user.trainee).filter(events__term=Term.current_term())), list(GroupSlip.objects.filter(trainee=self.request.user.trainee).filter(start__gte=Term.current_term().start).filter(end__lte=Term.current_term().end)))
         return context
 
-
-class RollViewSet(viewsets.ModelViewSet):
+class RollViewSet(BulkModelViewSet):
     queryset = Roll.objects.all()
     serializer_class = RollSerializer
-
-class AttendanceViewSet(viewsets.ModelViewSet):
-    queryset = Trainee.objects.all()
-    serializer_class = AttendanceSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filter_class = RollFilter
     def get_queryset(self):
         user = self.request.user
-        trainee=Trainee.objects.filter(account=user)
+        roll = Roll.objects.filter(trainee=user.trainee).filter(event__term=Term.current_term())
+        return roll
+    def allow_bulk_destroy(self, qs, filtered):
+        return not all(x in filtered for x in qs)
+
+class AttendanceViewSet(BulkModelViewSet):
+    queryset = Trainee.objects.all()
+    serializer_class = AttendanceSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filter_class = AttendanceFilter
+    def get_queryset(self):
+        user = self.request.user
+        trainee = Trainee.objects.filter(account=user)
         return trainee
-    
+    def allow_bulk_destroy(self, qs, filtered):
+        return not all(x in filtered for x in qs)
+
+class AllRollViewSet(BulkModelViewSet):
+    queryset = Roll.objects.all()
+    serializer_class = RollSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filter_class = RollFilter
+    def get_queryset(self):
+        queryset = Roll.objects.all()
+        data = self.request.get_full_path()
+        if ('_' not in data):
+            return queryset
+        data = data.split('_',1)[1]
+        data = data.split('&and_')
+        or_params = {}
+        and_params = {}
+        for key in data:
+            if '&or_' in key:
+                firstFilter=True
+                splitdata = key.split('&or_')
+                for splitdata2 in splitdata:
+                    or_data = splitdata2.split('=')
+                    if (or_data[1]=='True'):
+                        or_data[1] = True
+                    elif (or_data[1]=='False'):
+                        or_data[1] = False
+                    or_params[or_data[0]] = or_data[1]
+                    if firstFilter:
+                        queryset = queryset.filter(**or_params)
+                        firstFilter=False
+                    else:
+                        queryset = queryset | Roll.objects.filter(**or_params)
+                    or_params={}
+            else:
+                splitdata=key.split('=')
+                if (splitdata[1]=='True'):
+                    splitdata[1] = True
+                elif (splitdata[1]=='False'):
+                    splitdata[1] = False
+                and_params[splitdata[0]] = splitdata[1]
+                queryset = queryset.filter(**and_params)
+                and_params={}
+        return queryset
+    def allow_bulk_destroy(self, qs, filtered):
+        return not all(x in filtered for x in qs)
+
+class AllAttendanceViewSet(BulkModelViewSet):
+    queryset = Trainee.objects.all()
+    serializer_class = AttendanceSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filter_class = AttendanceFilter
+    def allow_bulk_destroy(self, qs, filtered):
+        return not all(x in filtered for x in qs)
