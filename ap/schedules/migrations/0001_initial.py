93ac4fbcd43202afef27a09a00c40d3b61b0a501
Ray Li <ray_li@mit.edu>
2016-04-08 12:27:05 -0700

    Successfully added new migrations for new models

--- ap/schedules/migrations/0001_initial.py
+++ ap/schedules/migrations/0001_initial.py
@@ -7,7 +7,7 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('accounts', '0006_trainingassistant_services'),
+        ('accounts', '0001_initial'),
     ]
 
     operations = [
