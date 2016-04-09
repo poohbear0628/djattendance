1d86fb25c6b662ed26af5ddda963f6d2fa104502
Hailey <hailey.lin@outlook.com>
2016-04-09 15:01:05 -0700

    redo migrations

--- ap/exams/migrations/0002_auto_20160409_1449.py
+++ ap/exams/migrations/0002_auto_20160409_1449.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+import datetime
 import django.contrib.postgres.fields.hstore
 
 
@@ -9,7 +10,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('classes', '0002_auto_20150627_2125'),
-        ('accounts', '0005_auto_20150905_1512'),
+        ('accounts', '0006_trainee_current_term'),
         ('exams', '0001_initial'),
     ]
 
@@ -18,14 +19,46 @@ class Migration(migrations.Migration):
             name='Exam',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('is_midterm', models.BooleanField()),
-                ('section_count', models.IntegerField(default=1)),
-                ('total_score', models.DecimalField(max_digits=5, decimal_places=2)),
+                ('name', models.CharField(max_length=30, blank=True)),
+                ('is_open', models.BooleanField(default=False)),
+                ('duration', models.DurationField(default=datetime.timedelta(0, 5400))),
+                ('category', models.CharField(max_length=1, choices=[(b'M', b'Midterm'), (b'F', b'Final')])),
+                ('total_score', models.DecimalField(default=0.0, max_digits=5, decimal_places=2)),
                 ('training_class', models.ForeignKey(to='classes.Class')),
             ],
         ),
         migrations.CreateModel(
-            name='ExamInstance',
+            name='Responses',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('responses', django.contrib.postgres.fields.hstore.HStoreField(null=True)),
+                ('score', models.DecimalField(max_digits=5, decimal_places=2)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Retake',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('is_complete', models.BooleanField(default=False)),
+                ('time_opened', models.DateTimeField(auto_now_add=True)),
+                ('exam', models.ForeignKey(to='exams.Exam')),
+                ('trainee', models.ForeignKey(to='accounts.Trainee')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Section',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('description', models.TextField(null=True, blank=True)),
+                ('section_index', models.IntegerField(default=0)),
+                ('first_question_index', models.IntegerField(default=1)),
+                ('question_count', models.IntegerField()),
+                ('questions', django.contrib.postgres.fields.hstore.HStoreField(null=True)),
+                ('exam', models.ForeignKey(related_name='sections', to='exams.Exam')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Session',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('is_submitted_online', models.BooleanField(default=True)),
@@ -37,37 +70,14 @@ class Migration(migrations.Migration):
                 ('trainee', models.ForeignKey(to='accounts.Trainee')),
             ],
         ),
-        migrations.CreateModel(
-            name='Response',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('question_index', models.IntegerField(default=1)),
-                ('response', django.contrib.postgres.fields.hstore.HStoreField(null=True)),
-                ('grader_meta', django.contrib.postgres.fields.hstore.HStoreField(null=True)),
-                ('score', models.DecimalField(max_digits=5, decimal_places=2)),
-                ('instance', models.ForeignKey(to='exams.ExamInstance')),
-                ('trainee', models.ForeignKey(to='accounts.Trainee')),
-            ],
+        migrations.AddField(
+            model_name='responses',
+            name='section',
+            field=models.ForeignKey(to='exams.Section'),
         ),
-        migrations.CreateModel(
-            name='Retake',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('is_complete', models.BooleanField(default=False)),
-                ('time_opened', models.DateTimeField(auto_now_add=True)),
-                ('exam_template', models.ForeignKey(to='exams.Exam')),
-                ('trainee', models.ForeignKey(to='accounts.Trainee')),
-            ],
-        ),
-        migrations.CreateModel(
-            name='Section',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('section_index', models.IntegerField(default=0)),
-                ('first_question_index', models.IntegerField(default=1)),
-                ('question_count', models.IntegerField()),
-                ('questions', django.contrib.postgres.fields.hstore.HStoreField(null=True)),
-                ('exam', models.ForeignKey(to='exams.Exam')),
-            ],
+        migrations.AddField(
+            model_name='responses',
+            name='session',
+            field=models.ForeignKey(to='exams.Session'),
         ),
     ]
