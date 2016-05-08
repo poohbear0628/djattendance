4fbfc38668c27bef6ce82cb2389dc2f256fb41c3
Hailey <hailey.lin@outlook.com>
2016-05-07 21:11:17 -0700

    update to context processor

--- ap/exams/context_processors.py
+++ ap/exams/context_processors.py
@@ -1,9 +1,4 @@
 from .models import Exam
 
 def exams_available(request):
-    exams = Exam.objects.all()
-    for exam in exams:
-        if exam.is_available(request.user.trainee):
-            return {'exam_available' : True}
-
-    return {'exam_available' : False}
+    return {'exams_available' : Exam.objects.filter(is_open=True) > 0}
