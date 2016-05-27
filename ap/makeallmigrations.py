9a16ddd9087b741e6ab0de2bf5d2e589f0c504e1
Ray Li <ray_li@mit.edu>
2016-05-26 17:47:29 -0700

    Added makeallmigrations file to auto make migration files for all apps

---
+++ ap/makeallmigrations.py
@@ -0,0 +1,9 @@
+from django.conf import settings
+import os
+
+if __name__ == '__main__':
+  apps = ' '.join([ app for app in settings.INSTALLED_APPS if not "django" in app ])
+  print 'Running make migrations on: ', apps
+  print '--------------------'
+
+  os.system('./manage.py makemigrations ' + apps)
