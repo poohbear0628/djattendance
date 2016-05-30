bc570bcfd9a5e81d85661596e329fd95d679b862
David Tien <david.h.tien@gmail.com>
2016-05-30 15:44:35 -0700

    fix path for real this time

--- ap/ap/settings/base.py
+++ ap/ap/settings/base.py
@@ -8,7 +8,7 @@ from django.contrib.messages import constants as message_constants
 # calculated paths for django and the site
 # used as starting points for various other paths
 DJANGO_ROOT = os.path.dirname(os.path.realpath(django.__file__))
-SITE_ROOT = os.path.dirname(os.path.abspath(__name__))
+SITE_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 
 
 ADMINS = (
@@ -71,7 +71,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(SITE_ROOT, 'ap', 'static'),
+    os.path.join(SITE_ROOT, 'static'),
 )
 
 # List of finder classes that know how to find static files in
@@ -121,7 +121,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(SITE_ROOT, 'ap', 'templates'),
+    os.path.join(SITE_ROOT, 'templates'),
 )
 
 DATABASES = {
