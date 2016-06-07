12c1c886a828659adf9513e4fae9fd4bd62323f7
Amaris Chen <amarisch@uw.edu>
2016-06-07 13:40:49 -0700

    delete php2django files that should not be in the repo

--- ap/local_settings.py
+++
@@ -1,5 +0,0 @@
-mysql_params = {
- 'host':  "localhost", # your host, usually localhost
- 'user':  "Monitor", # your username
- 'passwd':"chen", # your password
- 'db':    "officedb"} # the database name
