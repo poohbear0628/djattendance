d35fb44e5ea657ce9ea0da5c09fbf04143ef00b0
Ray <ray_li@mit.edu>
2016-06-09 14:11:56 -0700

    Finally fixed makeallmigrations path problem

--- ap/makeallmigrations.py
+++ ap/makeallmigrations.py
@@ -1,11 +1,9 @@
 from django.conf import settings
 import os
 
-SITE_ROOT = os.path.dirname(os.path.abspath(__name__))
-
 if __name__ == '__main__':
   apps = ' '.join([ app for app in settings.INSTALLED_APPS if not "django" in app ])
   print 'Running make migrations on: ', apps
   print '--------------------'
 
-  os.system(os.path.join(SITE_ROOT, 'ap', 'manage.py') + ' makemigrations ' + apps)
+  os.system(os.path.join(settings.SITE_ROOT, 'manage.py') + ' makemigrations ' + apps)
