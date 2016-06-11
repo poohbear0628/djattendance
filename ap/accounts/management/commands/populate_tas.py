6e6669c2a32a09f8d482ccdd226893606e703e71
David Tien <david.h.tien@gmail.com>
2016-06-11 15:35:08 -0700

    add some management scripts

---
+++ ap/accounts/management/commands/populate_tas.py
@@ -0,0 +1,23 @@
+from django.core.management.base import BaseCommand
+from accounts.models import User
+
+class Command(BaseCommand):
+    # to use: python ap/manage.py populate_tas --settings=ap.settings.dev
+    def _create_tas(self):
+        # change the array below to get different tas
+        tas = ['Andrew Li', 'Jerome Keh', 'Joseph Bang', 'Paul Deng', 'Walt Hale']
+        for ta in tas:
+            print ta
+            email = '.'.join(ta.lower().split(' ')) + "@lsm.org"
+            firstname = ta.split(' ')[0]
+            lastname = ta.split(' ')[1]
+            gender = 'B'
+            password = 'ap'
+            date_of_birth = '1974-12-12'
+            u = User(email=email, firstname=firstname, lastname=lastname, gender=gender, password=password, type='T', date_of_birth=date_of_birth, is_staff=True, is_admin=True)
+            u.save()
+
+        print 'done'
+
+    def handle(self, *args, **options):
+        self._create_tas()
