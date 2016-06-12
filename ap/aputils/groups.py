bc09da866956561908ad8d8dc84d5020324e8706
Lisa Dina <lisadina@ADs-MacBook-Air.local>
2016-06-11 21:13:45 -0700

    aputils/groups.py added

---
+++ ap/aputils/groups.py
@@ -0,0 +1,63 @@
+from django.contrib.auth.models import Group, Permission
+from django.contrib.contenttypes.models import ContentType
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
+
+#Adds all the permissions for the listed models
+def model_permissions(group_name, model_name_list):
+	for model_name in model_name_list:
+		ct = ContentType.objects.get(model=model_name)
+		permission_list = Permission.objects.filter(content_type=ct)
+		for permission in permission_list:
+			group_name.permissions.add(permission)
+
+#Adds all the permissions for the listed apps
+def app_permissions(group_name, app_label_list):
+	for app_label in app_name_list:
+		cts = ContentType.objects.filter(app_label=app_label)
+		for ct in cts:
+			permission_list = Permission.objects.filter(content_type=ct)
+			for permission in permission_list:
+				group_name.permissions.add(permission)
+
+
+
+@receiver(post_migrate)
+def add_group_permissions(sender, **kwargs):
+	#This permission group includes the permanent Training Assistant (TA) brothers, helper TA brothers, TA sisters, brother Dennis Higashi, and brother Curt Kennard. They can see everything except for developer tools. 
+	#TODO: Add all these permissions
+	AdminGroup, created = Group.objects.get_or_create(name="administration")
+	if created:
+		model_permissions(AdminGroup, ['roll'])
+	MaintenanceGroup = Group.objects.get_or_create(name="maintenance")
+	#Do I need a general trainee permission group?
+	#DONE
+	AbsentTraineeRosterGroup, created = Group.objects.get_or_create(name="absent_trainee_roster")
+	if created:
+		app_permissions(AbsentTraineeRosterGroup, ['absent_trainee_roster'])
+		# model_permissions(AbsentTraineeRosterGroup, ['absentees', 'entries', 'rosters'])
+
+	AttendanceMonitorGroup, created = Group.objects.get_or_create(name="attendance_monitors")
+	if created:
+		app_permissions(AttendanceMonitorGroup, ['attendance', 'leaveslips', 'seating', 'schedules'])
+		# model_permissions(AbsentTraineeRosterGroup, ['absentees', 'entries', 'rosters', 'group_slip', 'individual_slip','seating'])
+	AVGroup = Group.objects.get_or_create(name="av")
+	DeveloperGroup = Group.objects.get_or_create(name="dev")
+	ExamGraderGroup = Group.objects.get_or_create(name="exam_graders")
+	GradCommitteeGroup = Group.objects.get_or_create(name="grad_committee")
+	HCGroup = Group.objects.get_or_create(name="HC")
+	FFLGroup = Group.objects.get_or_create(name="facility_maintenance_or_frames_or_linens")
+	HouseInsepectorGroup = Group.objects.get_or_create(name="house_inspectors")
+	SATestingGroupsCoordinatorsGroup = Group.objects.get_or_create(name="semi_annual_testing_group_coordinators")
+	ServiceSchedulerGroup = Group.objects.get_or_create(name="service_schedulers")
+	TeamMonitorGroup = Group.objects.get_or_create(name="team_monitors")
+	YPCMonitorGroup = Group.objects.get_or_create(name="ypc_monitors")
+	XBTrainee = Group.objects.get_or_create(name="xb_trainees")
+	#4th term trainees. Do we need to make a group for that? 
+	#All designated service trainees should have that perm
+	#2nd year trainees to submit personal attendance
+	SpecialProjectsGroup = Group.objects.get_or_create(name="special_projects")
+	OfficeSupportGroup = Group.objects.get_or_create(name="office_support")
+	BadgesGroup = Group.objects.get_or_create(name="badges")
+	HealthOfficeGroup = Group.objects.get_or_create(name="health_office")
+	KitchenGroup = Group.objects.get_or_create(name="kitchen")
