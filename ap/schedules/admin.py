1def36ac1349086c60e6c8b316f96acd1a9abf61
[Sam] <[sam.chai0501@gmail.com]>
2016-06-30 19:58:48 -0700

    fix indentation in schedules admin

--- ap/schedules/admin.py
+++ ap/schedules/admin.py
@@ -3,19 +3,19 @@ from schedules.models import *
 from .models import Event, Schedule, Class
 
 class ClassAdmin(admin.ModelAdmin):
- exclude = ['type']
+    exclude = ['type']
 
- # Automatically type class event objects saved.
- def save_model(self, request, obj, form, change):
-   obj.type = 'C'
-   obj.save()
+    # Automatically type class event objects saved.
+    def save_model(self, request, obj, form, change):
+        obj.type = 'C'
+        obj.save()
 
 
 class EventAdmin(admin.ModelAdmin):
-   list_display = ("name", "description", "type", "start", "end", "day", "weekday", "chart")
+    list_display = ("name", "description", "type", "start", "end", "day", "weekday", "chart")
 
 class ScheduleAdmin(admin.ModelAdmin):
-   list_display = ("name", "comments", "priority", "term", "season", "weeks", "is_deleted")
+    list_display = ("name", "comments", "priority", "term", "season", "weeks", "is_deleted")
 
 admin.site.register(Event, EventAdmin)
 admin.site.register(Schedule, ScheduleAdmin)
