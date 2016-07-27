d6bd59a7b1e6530dbc0d0dd2a08fe7e16fd132a9
Ray <ray_li@mit.edu>
2016-07-27 11:20:00 -0700

    Consolidated class models, rewrote much of exam frontend, performance tweak as well

--- ap/schedules/admin.py
+++ ap/schedules/admin.py
@@ -1,15 +1,7 @@
 from django import forms
 from django.contrib import admin
 from schedules.models import *
-from .models import Event, Schedule, Class
-
-class ClassAdmin(admin.ModelAdmin):
-  exclude = ['type']
-
-  # Automatically type class event objects saved.
-  def save_model(self, request, obj, form, change):
-      obj.type = 'C'
-      obj.save()
+from .models import Event, Schedule#, Class
 
 
 class EventAdmin(admin.ModelAdmin):
@@ -48,7 +40,5 @@ class ScheduleAdmin(admin.ModelAdmin):
   list_display = ("name", "comments", "priority", "term", "season", "weeks", "is_deleted")
   registered_filtered_select = [('trainees', Trainee), ('events', Event)]
 
-
 admin.site.register(Event, EventAdmin)
 admin.site.register(Schedule, ScheduleAdmin)
-admin.site.register(Class, ClassAdmin)
