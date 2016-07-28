043e530587d99621f31d59c6abc5f6e2051010eb
Ray <ray_li@mit.edu>
2016-07-27 20:06:27 -0700

    Added permissions. Got overview + reports working

--- ap/exams/urls.py
+++ ap/exams/urls.py
@@ -9,5 +9,8 @@ urlpatterns = patterns(
     url(r'^(?P<pk>\d+)/edit$', views.ExamEditView.as_view(), name='edit'),
     url(r'^(?P<pk>\d+)/take$', views.TakeExamView.as_view(), name='take'),
     url(r'^(?P<pk>\d+)/grade$', views.GradeExamView.as_view(), name='grade'),
-    url(r'^(?P<pk>\d+)/grades$', views.SingleExamGradesListView.as_view(), name='grades')    
+    url(r'^(?P<pk>\d+)/grades$', views.SingleExamGradesListView.as_view(), name='grades'),
+    url(r'^(?P<pk>\d+)/overview$', views.GenerateOverview.as_view(), name='overview'),
+    url(r'report/(?P<pk>\d*)$', views.GenerateGradeReports.as_view(), name='report'),
+    url(r'report/$', views.GenerateGradeReports.as_view(), name='report-all')
 )
