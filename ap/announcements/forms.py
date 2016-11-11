2384e12d4803dff71cd325d831aa7f72b14ff8cf
billyeh <billwyeh@yahoo.com>
2016-11-11 12:09:48 -0800

    rather use datepicker for announcements

--- ap/announcements/forms.py
+++ ap/announcements/forms.py
@@ -1,12 +1,17 @@
 from django import forms
 
-from bootstrap3_datetime.widgets import DateTimePicker
-
 from .models import Announcement
 
+from functools import partial
+
+DateInput = partial(forms.DateInput, {'class': 'datepicker'})
+
 class AnnouncementForm(forms.ModelForm):
+
+    announcement_date = forms.DateField(widget=DateInput())
+    announcement_end_date = forms.DateField(widget=DateInput())
+
     class Meta:
         model = Announcement
         fields = ('type', 'announcement', 'announcement_date', 'announcement_end_date')
-        widgets = {'announcement_date': DateTimePicker(options={'format': 'MM/DD/YYYY HH:mm'}),
-                   'announcement_end_date': DateTimePicker(options={'format': 'MM/DD/YYYY HH:mm'})}
+
