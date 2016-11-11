332c4e613af92a639607ed62b6ff6faca3d3fa35
billyeh <billwyeh@yahoo.com>
2016-11-10 21:46:30 -0800

    create should be at beginning of url

--- ap/announcements/urls.py
+++ ap/announcements/urls.py
@@ -5,6 +5,6 @@ from announcements import views
 app_name = 'announcements'
 
 urlpatterns = [
-    url(r'create/$', views.AnnouncementRequest.as_view(), name='announcement-request'),
+    url(r'^create/$', views.AnnouncementRequest.as_view(), name='announcement-request'),
     url(r'^$', views.AnnouncementRequestList.as_view(), name='announcement-request-list')
 ]
