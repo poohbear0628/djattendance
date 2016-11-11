83af276568a70c008b0f76b082678273da241ae7
billyeh <billwyeh@yahoo.com>
2016-11-10 20:42:09 -0800

    redirect after creating announcement

--- ap/announcements/views.py
+++ ap/announcements/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
+from django.contrib.messages.views import SuccessMessageMixin
 
 from bootstrap3_datetime.widgets import DateTimePicker
 
 from .models import Announcement
 from .forms import AnnouncementForm
 
-class AnnouncementRequest(CreateView):
+class AnnouncementRequest(SuccessMessageMixin, CreateView):
     template_name = 'announcement_request.html'
     form_class = AnnouncementForm
+    success_url = '/'
+    success_message = 'Announcement requested successfully'
