def00358613d77b261676b3568f5caf2ff678b33
billyeh <billwyeh@yahoo.com>
2016-11-10 21:42:10 -0800

    make announcements list page

--- ap/announcements/views.py
+++ ap/announcements/views.py
@@ -1,14 +1,29 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
+from django.views import generic
 from django.contrib.messages.views import SuccessMessageMixin
 
 from bootstrap3_datetime.widgets import DateTimePicker
 
+from aputils.trainee_utils import trainee_from_user
+
 from .models import Announcement
 from .forms import AnnouncementForm
 
-class AnnouncementRequest(SuccessMessageMixin, CreateView):
+class AnnouncementRequest(SuccessMessageMixin, generic.edit.CreateView):
     template_name = 'announcement_request.html'
     form_class = AnnouncementForm
     success_url = '/'
     success_message = 'Announcement requested successfully'
+
+class AnnouncementRequestList(generic.ListView):
+    model = Announcement
+    template_name = 'announcement_list.html'
+
+    def get_queryset(self):
+        trainee = trainee_from_user(self.request.user)
+        if trainee:
+            return Announcement.objects.filter(trainee=trainee).order_by('status')
+        else:
+            return Announcement.objects.filter().order_by('status')
+
