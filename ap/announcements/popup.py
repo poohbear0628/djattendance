6c0f8c43bc21db168c8d8f2a2f3800df8abbb6c5
billyeh <billwyeh@yahoo.com>
2016-12-08 20:31:07 -0800

    add popup if trainees are using server during class and move popups to template folder

---
+++ ap/announcements/popup.py
@@ -0,0 +1,16 @@
+from aputils.trainee_utils import is_trainee, trainee_from_user
+
+def class_popup(request):
+    data = {}
+    if is_trainee(request.user):
+        trainee = trainee_from_user(request.user)
+        events = trainee.immediate_upcoming_event(time_delta=0)
+        if not events:
+            data['is_in_class'] = False
+            return data
+        event = events[0]
+        if event.type == 'C':
+            data['is_in_class'] = True
+        else:
+            data['is_in_class'] = False
+    return data
