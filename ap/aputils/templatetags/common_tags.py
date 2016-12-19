cb5717c5d4b83b85869724f0212cece60345c620
billyeh <billwyeh@yahoo.com>
2016-12-18 16:21:02 -0800

    remove resolve_variable

--- ap/aputils/templatetags/common_tags.py
+++ ap/aputils/templatetags/common_tags.py
@@ -1,6 +1,6 @@
 from django import template
 from aputils.trainee_utils import is_trainee, is_TA
-from django.template import resolve_variable, NodeList
+from django.template import NodeList, Variable
 from django.contrib.auth.models import Group
 
 register = template.Library()
@@ -42,10 +42,7 @@ class GroupCheckNode(template.Node):
         self.nodelist_true = nodelist_true
         self.nodelist_false = nodelist_false
     def render(self, context):
-        user = resolve_variable('user', context)
-
-        if not user.is_authenticated():
-            return self.nodelist_false.render(context)
+        user = Variable('user').resolve(context)
 
         for group in self.group.split("|"):
             group = group[1:-1] if group.startswith('"') and group.endswith('"') else group
