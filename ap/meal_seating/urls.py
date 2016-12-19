222bb09524347d99b034d715416be171b91bf2cd
billyeh <billwyeh@yahoo.com>
2016-12-18 16:27:44 -0800

    fix urls in meal_seating

--- ap/meal_seating/urls.py
+++ ap/meal_seating/urls.py
@@ -1,16 +1,16 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from meal_seating import views
 
-urlpatterns = patterns('',
-    url(r'^$',
-        views.newseats),
-    url(r'viewlist/',
-        views.seattables),
-    url(r'mealsignin/',
-        views.signin),
-    url(r'editinfo/',
-        views.editinfo),
-    url(r'tables/', 
-        views.TableListView.as_view(), name='table_edit'),
-)
+urlpatterns = [
+  url(r'^$',
+    views.newseats),
+  url(r'viewlist/',
+    views.seattables),
+  url(r'mealsignin/',
+    views.signin),
+  url(r'editinfo/',
+    views.editinfo),
+  url(r'tables/',
+    views.TableListView.as_view(), name='table_edit'),
+]
