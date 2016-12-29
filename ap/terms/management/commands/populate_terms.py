b5d9acac932e80f310bbc6c689e9e5f8ad018729
billyeh <billwyeh@yahoo.com>
2016-12-29 15:13:42 -0800

    use existing functions

--- ap/terms/management/commands/populate_terms.py
+++ ap/terms/management/commands/populate_terms.py
@@ -1,44 +1,26 @@
 from django.core.management.base import BaseCommand
+
 from terms.models import Term
+from apimport.utils import term_start_date_from_semiannual, term_end_date_from_semiannual, currently_in_term, create_term
 
 from datetime import date
 import calendar
 from dateutil.relativedelta import relativedelta
-
-def date_for_day_of_week(date, day):
-  """ returns the date of the specified day in the week identified by date.
-      For example, if date 4/9/2016 (Saturday) and 0 (Monday) are passed in,
-      4/4/2016 will be returned. """
-  return date + relativedelta(days=day-date.weekday())
-
-def is_current_term(start, end):
-  return date.today() >= start and date.today() <= end
 
 class Command(BaseCommand):
   # to use: python ap/manage.py populate_terms --settings=ap.settings.dev
 
   def _create_terms(self):
     today = date.today()
-    monday, saturday = (0, 5)
-    term_length_minus_semiannual = 20 - 1
     for year in range(2010, today.year + 1):
-      july_fourth = date(year, 7, 4)
-      christmas = date(year, 12, 25)
-      # Because Monday == 0, if these are on Sunday, we incorrectly get the previous week, so adjust
-      if christmas.weekday() == 6:
-        christmas += relativedelta(days=1)
-      if july_fourth.weekday() == 6:
-        july_fourth += relativedelta(days=1)
-      spring_semiannual_monday = date_for_day_of_week(july_fourth, monday)
-      spring_start = spring_semiannual_monday + relativedelta(weeks=-term_length_minus_semiannual)
-      spring_end = date_for_day_of_week(july_fourth, saturday)
-      fall_semiannual_monday = date_for_day_of_week(christmas, monday)
-      fall_start = fall_semiannual_monday + relativedelta(weeks=-term_length_minus_semiannual)
-      fall_end = date_for_day_of_week(christmas, saturday)
-      fall = Term(current=is_current_term(fall_start, fall_end), season='Fall', year=year, start=fall_start, end=fall_end)
-      fall.save()
-      spring = Term(current=is_current_term(spring_start, spring_end), season='Spring', year=year, start=spring_start, end=spring_end)
-      spring.save()
+      seasons_to_semiannual = {'Fall': 'Winter', 'Spring': 'Summer'}
+      for season, semiannual in seasons_to_semiannual.items():
+        term_start = term_start_date_from_semiannual(semiannual, year)
+        term_end = term_end_date_from_semiannual(semiannual, year)
+        in_term = currently_in_term(term_start, term_end)
+        create_term(season, year, term_start, term_end)
+        if in_term: # we've reached our current term, so stop
+          break
 
   def handle(self, *args, **options):
     self._create_terms()
