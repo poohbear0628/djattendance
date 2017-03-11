102768e9d0ca64b5f13e27cc706b1f36509a7ded
Bill Yeh <billwyeh@gmail.com>
2017-03-11 13:45:36 -0800

    initial house request models

--- ap/house_requests/models.py
+++ ap/house_requests/models.py
@@ -1,8 +1,26 @@
 from django.db import models
 
-class RequestInterface:
+from houses.models import House
+from accounts.models import Trainee
+
+class HouseRequest(models.Model):
+  class Meta:
+    abstract = True
+
+  STATUS = (
+    ('A', 'Approved'),
+    ('P', 'Pending'),
+    ('F', 'Fellowship'),
+    ('D', 'Denied'),
+  )
+
+  status = models.CharField(max_length=1, choices=STATUS, default='P')
+  date_requested = models.DateTimeField(auto_now_add=True)
+  trainee_author = models.ForeignKey(Trainee, null=True)
+  TA_comments = models.TextField(null=True, blank=True)
+
   def get_category(self):
-    return self.get_type_display()
+    return self.type
   def get_status(self):
     return self.get_status_display()
   def get_date_created(self):
@@ -10,28 +28,20 @@ class RequestInterface:
   def get_trainee_requester(self):
     return self.trainee_author
 
-  @staticmethod
-  def create_context(cls, isTA):
-    return {
-      'item_name': cls._meta.verbose_name,
-      'create_url': cls.get_create_url(),
-      'is_TA': isTA,
-    }
+class MaintenanceRequest(HouseRequest):
+  type = 'Maintenance'
+  description = models.TextField()
+  house = models.ForeignKey(House, related_name='maintenance_requests')
 
-  @staticmethod
-  def get_create_url():
-    raise NotImplementedError
-  def get_absolute_url(self):
-    raise NotImplementedError
-  @staticmethod
-  def get_detail_template():
-    raise NotImplementedError
-  @staticmethod
-  def get_ta_button_template():
-    raise NotImplementedError
-  @staticmethod
-  def get_button_template():
-    raise NotImplementedError
-  @staticmethod
-  def get_table_template():
-    raise NotImplementedError
+class LinensRequest(HouseRequest):
+  type = 'Linens'
+  item = models.TextField()
+  quantity = models.PositiveSmallIntegerField()
+  reason = models.TextField()
+  house = models.ForeignKey(House, related_name='linens_requests')
+
+class FramingRequest(HouseRequest):
+  type = 'Framing'
+  location = models.TextField()
+  frame = models.TextField()
+  house = models.ForeignKey(House, related_name='framing_requests')
