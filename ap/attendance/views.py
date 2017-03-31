3d8477b717b71d672ee6077d99aceb3099dbd146
Bill Yeh <billwyeh@yahoo.com>
2017-03-31 15:26:14 -0700

    fix allattendance api'

--- ap/attendance/views.py
+++ ap/attendance/views.py
@@ -321,9 +321,12 @@ class AttendanceViewSet(BulkModelViewSet):
   filter_backends = (filters.DjangoFilterBackend,)
   # filter_class = AttendanceFilter
   def get_queryset(self):
-    user = self.request.user
-    trainee = trainee_from_user(user)
-    return trainee
+    if 'trainee' in self.request.GET:
+      trainee = Trainee.objects.get(pk=self.request.GET.get('trainee'))
+    else:
+      user = self.request.user
+      trainee = trainee_from_user(user)
+    return [trainee]
   def allow_bulk_destroy(self, qs, filtered):
     return not all(x in filtered for x in qs)
 
