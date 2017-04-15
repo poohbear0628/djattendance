e99ae6443db450c6188e338cb3d7ccb211ad01e7
samchai <sam.chai0501@gmail.com>
2017-04-15 09:02:22 -0700

    displaying unexcused absences with slip status

--- ap/static/react/scripts/components/RollDetail.js
+++ ap/static/react/scripts/components/RollDetail.js
@@ -1,21 +1,29 @@
 import React, { PropTypes } from 'react'
 
 const RollDetail = ({event,roll}) => {
-  let roll_status = ''
-  if (event.status.roll === 'absent' && (event.status.slip === 'unexcused' || event.status.slip === 'pending')) {
-    roll_status = 'roll__table roll__table--absent row'
-  } else if (event.status.roll === 'tardy' && (event.status.slip === 'unexcused' || event.status.slip === 'pending')) {
-    roll_status = 'roll__table roll__table--tardy row'
+  let roll_display = ''
+  let roll_status = event.status.roll
+  let slip_status = event.status.slip
+  if (roll_status === 'absent' && (slip_status === 'unexcused' || slip_status === 'pending')) {
+    roll_display = 'roll__table roll__table--absent row'
+  } else if (roll_status === 'tardy' && (slip_status === 'unexcused' || slip_status === 'pending')) {
+    roll_display = 'roll__table roll__table--tardy row'
   } else {
-    roll_status = 'roll__table roll__table--present row'
+    roll_display = 'roll__table roll__table--present row'
   }
   return (
-  	<div className={roll_status} key={event['id']}> 
-    	<div className="col-md-4" key={event['start']}>
+  	<div className={roll_display} key={event['id']}> 
+    	<div className="col-md-2" key={event['start']}>
         {dateFns.format(event['start'], 'M/D ddd')}
       </div>
-      <div className="col-md-8" key={event['name']}>
+      <div className="col-md-4" key={event['name']}>
       	{event["name"]}
+      </div>
+      <div className="col-md-3" key={roll_status}>
+        {roll_status}
+      </div>
+      <div className="col-md-3" key={slip_status}>
+        {slip_status}
       </div>
     </div>
   )
