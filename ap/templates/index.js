19ae4140580208d67c97c76bfa34be827f6723cc
Bill Yeh <billwyeh@yahoo.com>
2017-05-10 20:49:59 -0700

    reorganize index.js, use expose-loader

--- ap/templates/index.js
+++ ap/templates/index.js
@@ -7,71 +7,61 @@
  * speed up site load.
  */
 
-window.moment = require('moment');
-
-
-var $ = require('jquery');
-//Expose jQuery to global
-window.$ = $;
-window.jQuery = $;
-
-require('jquery.shorten/src/jquery.shorten');
-require('devbridge-autocomplete');
-
-require('bootstrap-sass');
+/**************************
+* expose global libraries *
+**************************/
+require('expose-loader?moment!moment');
+require('expose-loader?$!expose-loader?jQuery!jquery');
 require('expose-loader?Countable!countable');
+require('expose-loader?Dropzone!dropzone'); require('dropzone/dist/dropzone.css');
+require('expose-loader?_!underscore');
+require('expose-loader?xolor!xolor');
 
+/*******
+* init *
+*******/
 // Add fast click to remove click delay on mobile
 var attachFastClick = require('fastclick');
 attachFastClick.attach(document.body);
-
-// Datatables import
-// https://datatables.net/forums/discussion/39648/datatable-buttons-not-working-on-webpack
-import "datatables.net";
-import "datatables.net-bs";
-import "datatables.net-buttons";
-import "datatables.net-buttons-bs";
-import 'datatables.net-buttons/js/buttons.colVis.js';
-import 'datatables.net-buttons/js/buttons.html5.js';
-import 'datatables.net-buttons/js/buttons.flash.js';
-import 'datatables.net-buttons/js/buttons.print.js';
-import 'datatables.net-select';
-import 'datatables.net-fixedheader';
-import 'datatables.net-scroller';
-
-// PDFmake for pdf export datatables
-require('pdfmake/build/pdfmake');
-require('pdfmake/build/vfs_fonts.js');
-
 var autosize = require('autosize/dist/autosize.js');
 $(document).ready(function(){
   // Makes all textarea elastic (resize according to content)
   autosize($('textarea'));
 });
+require('../static/libraries/bootstrap-select2/select2-django.js');
 
-// CSS imports
-import 'bootstrap-sass/assets/stylesheets/_bootstrap.scss';
-import 'datatables.net-bs/css/dataTables.bootstrap.css';
-import 'datatables.net-buttons-bs/css/buttons.bootstrap.css';
-import 'datatables.net-select-bs/css/select.bootstrap.css';
-import 'datatables.net-fixedheader-bs/css/fixedHeader.bootstrap.css';
-import 'datatables.net-scroller-bs/css/scroller.bootstrap.css';
-import 'bootflat-ftta/bootflat/scss/bootflat.scss'
-
-// Select2 imports
-import '../static/libraries/bootstrap-select2/select2-bootstrap.css';
-import '../static/libraries/bootstrap-select2/select2-django.js';
+/**************
+* CSS imports *
+**************/
+require('datatables.net-bs/css/dataTables.bootstrap.css');
+require('datatables.net-buttons-bs/css/buttons.bootstrap.css');
+require('datatables.net-select-bs/css/select.bootstrap.css');
+require('datatables.net-fixedheader-bs/css/fixedHeader.bootstrap.css');
+require('datatables.net-scroller-bs/css/scroller.bootstrap.css');
+require('bootstrap-sass/assets/stylesheets/_bootstrap.scss');
+require('bootflat-ftta/bootflat/scss/bootflat.scss');
+require('../static/libraries/bootstrap-select2/select2-bootstrap.css');
 require('select2/select2.css');
 
-window._ = require('underscore');
-
-window.Dropzone = require('dropzone');
-require('dropzone/dist/dropzone.css');
-
-require('selectize');
-require('selectize/dist/css/selectize.default.css');
-
-require('bootstrap-toggle');
-require('bootstrap-toggle/css/bootstrap-toggle.css');
-
-window.xolor = require('xolor');
+/***************
+* load plugins *
+****************/
+require('jquery.shorten/src/jquery.shorten');
+require('devbridge-autocomplete');
+require('selectize'); require('selectize/dist/css/selectize.default.css');
+require('bootstrap-sass');
+require('bootstrap-toggle'); require('bootstrap-toggle/css/bootstrap-toggle.css');
+require('pdfmake/build/pdfmake');
+require('pdfmake/build/vfs_fonts.js');
+// Datatables import, see https://datatables.net/forums/discussion/39648/datatable-buttons-not-working-on-webpack
+require("datatables.net");
+require("datatables.net-bs");
+require("datatables.net-buttons");
+require("datatables.net-buttons-bs");
+require('datatables.net-buttons/js/buttons.colVis');
+require('datatables.net-buttons/js/buttons.html5');
+require('datatables.net-buttons/js/buttons.flash');
+require('datatables.net-buttons/js/buttons.print');
+require('datatables.net-select');
+require('datatables.net-fixedheader');
+require('datatables.net-scroller');
