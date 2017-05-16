c84a9d8402a5da614cfbecc900fc1abfd5551674
Bill Yeh <billwyeh@yahoo.com>
2017-05-16 09:06:20 -0700

    Revert "I don't think we use dotfiles for env, see link"
    This reverts commit 0e69dc55ccc36244adf7beeb1a15a5472949df40.

--- ap/manage.py
+++ ap/manage.py
@@ -3,6 +3,16 @@ import os
 import sys
 
 if __name__ == "__main__":
+  ##################### Loading in env from apenv if file exists ##################
+  if 'APENV' in os.environ:
+    from aputils import dotenv
+    env_f = os.environ['APENV']
+    assert os.path.isfile(env_f), 'APENV path not set in env!'
+    print 'loading env file (overrides default)'
+    dotenv.read_dotenv(env_f)
+  else:
+    print '!!! WARNING !!! No apenv file found, using bash-supplied env (default)'
+
   ##################### default manage.py code below #################
 
   os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ap.settings.base")
