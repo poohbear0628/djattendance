5e83616a836ffbb0b71370f5dd0e83aff530fbf0
Bill Yeh <billwyeh@yahoo.com>
2017-05-16 10:05:35 -0700

    add braces

--- ap/static/js/bootstrap-tab-history.js
+++ ap/static/js/bootstrap-tab-history.js
@@ -1,6 +1,8 @@
 // Makes tabs url history aware.
 $(document).ready(function() {
-  if (location.hash !== '') $('a[href="' + location.hash + '"]').tab('show');
+  if (location.hash !== '') {
+    $('a[href="' + location.hash + '"]').tab('show');
+  }
   // add a hash to the URL when the user clicks on a tab
   $('a[data-toggle="tab"]').on('click', function(e) {
     history.pushState(null, null, $(this).attr('href'));
