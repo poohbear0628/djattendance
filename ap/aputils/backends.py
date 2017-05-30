e7135c1f09c1f5b3f524b85ea1fb635d0a569da6
Bill Yeh <billwyeh@yahoo.com>
2017-05-30 14:38:03 -0700

    fix indentation

--- ap/aputils/backends.py
+++ ap/aputils/backends.py
@@ -3,6 +3,6 @@ from django.contrib.auth.backends import ModelBackend
 
 class CaseInsensitiveModelBackend(ModelBackend):
   def authenticate(self, username=None, password=None, **kwargs):
-  	if username:
-    	username = username.lower()
+    if username:
+      username = username.lower()
     return super(CaseInsensitiveModelBackend, self).authenticate(username, password, **kwargs)
