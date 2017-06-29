8549cdaaa634d4bb41b280f948ab40168a30a7f4
Bill Yeh <billwyeh@yahoo.com>
2017-06-29 16:08:36 -0700

    fix a/v week and admin trainee select

--- ap/accounts/widgets.py
+++ ap/accounts/widgets.py
@@ -3,13 +3,11 @@ from django.utils.safestring import mark_safe
 from django.template.loader import render_to_string
 from ap.forms import TraineeSelectForm
 
-
 class TraineeSelect2MultipleInput(Select2MultipleWidget):
-  def render(self, name, value, attrs=None, choices=()):
-    output = super(TraineeSelect2MultipleInput, self).render(name, value, attrs, choices)
+  def render(self, name, value, attrs=None):
+    output = super(TraineeSelect2MultipleInput, self).render(name, value, attrs)
     button = '<button type="button" class="btn btn-default" data-toggle="modal" data-target="#trainee_select"><span class="hide-if-in-class">Add Trainee Group</span></button>'
     select_modal = render_to_string('popups/trainee_select_modal.html', {'trainee_select_form': TraineeSelectForm()})
-    print select_modal
     return output + mark_safe(button) + mark_safe(select_modal)
 
   class Media:
