7126f1213c3c38e4be4ac518a587e0be469d637b
samchai <sam.chai0501@gmail.com>
2017-06-29 20:10:04 -0700

    add trainee filter for admin rolls

--- ap/attendance/admin.py
+++ ap/attendance/admin.py
@@ -1,5 +1,51 @@
+from django import forms
 from django.contrib import admin
+from schedules.models import *
 from .models import Roll
 
-admin.site.register(Roll)
+from django.contrib.admin.widgets import FilteredSelectMultiple
+from django_select2.forms import ModelSelect2MultipleWidget
 
+from aputils.admin_utils import FilteredSelectMixin
+from aputils.custom_fields import CSIMultipleChoiceField
+
+from terms.models import Term
+from accounts.widgets import TraineeSelect2MultipleInput
+
+class RollForm(forms.ModelForm):
+  event = forms.ModelMultipleChoiceField(
+    label='Events',
+    queryset=Event.objects.all(),
+    required=False,
+    widget=admin.widgets.FilteredSelectMultiple(
+      "events", is_stacked=False))
+
+  trainee = forms.ModelChoiceField(
+    queryset=Trainee.objects.all(),
+    label='Participating Trainees',
+    required=False,
+    widget=TraineeSelect2MultipleInput,
+    )
+
+  def save(self, commit=True):
+    instance = super(RollForm, self).save(commit=False)
+    if commit:
+        instance.save()
+    return instance
+
+  def __init__(self, *args, **kwargs):
+    super(RollForm, self).__init__(*args, **kwargs)
+    self.fields['trainee'].widget.attrs = {'style':'width:100%'}
+
+  class Meta:
+    model = Roll
+    exclude = []
+
+# Works without mixin b/c relationship is explicitly defined
+class RollAdmin(admin.ModelAdmin):
+  form = RollForm
+  save_as = True
+  list_display = ("status", "finalized", "notes", "submitted_by", "date")
+  registered_filtered_select = [('trainees', Trainee), ('events', Event)]
+
+admin.site.register(Roll, RollAdmin)
