b62ad3b8bf4fa9f0398cd7a591631b5cebb812be
iamsitting <cdsalamanca27@gmail.cok>
2017-06-29 21:15:09 -0700

    Added TimeSelect to form

--- ap/room_reservations/forms.py
+++ ap/room_reservations/forms.py
@@ -1,11 +1,13 @@
 from django import forms
 
 from .models import RoomReservation
+from aputils.widgets import SelectTimeWidget
 
 class RoomReservationForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(RoomReservationForm, self).__init__(*args, **kwargs)
-
+        self.fields['start'].widget = SelectTimeWidget(twelve_hr=True, use_seconds=False, minute_step=30, required=True)
+        self.fields['end'].widget = SelectTimeWidget(twelve_hr=True, use_seconds=False, minute_step=30, required=True)
     class Meta:
         model = RoomReservation
         fields = ['group', 'date', 'start', 'end', 'room', 'group_size', 'frequency', 'reason']
