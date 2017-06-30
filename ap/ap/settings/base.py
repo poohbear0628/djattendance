d6e1ea39fca2145cffd6b727efd7c73643b878a9
samchai <sam.chai0501@gmail.com>
2017-06-30 07:28:56 -0700

    add django_filters to installed_apps

--- ap/ap/settings/base.py
+++ ap/ap/settings/base.py
@@ -214,6 +214,7 @@ INSTALLED_APPS = (
   'rest_framework',  # for API
   'django_countries', #to replace aputils country
   'localflavor', #to replace aputils states
+  'django_filters',
 
   # django wiki modules
   'django.contrib.humanize',
