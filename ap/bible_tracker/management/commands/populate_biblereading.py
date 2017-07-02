bd2cf94100c1e90b3d134bb0ce211851e4416d22
Benjamin Liu <yipingbenjamin.l@gmail.com>
2017-07-01 18:23:38 -0700

    randomizer for population script to simulate real-life data

--- ap/bible_tracker/management/commands/populate_biblereading.py
+++ ap/bible_tracker/management/commands/populate_biblereading.py
@@ -2,26 +2,30 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth.models import Group
 from bible_tracker.models import BibleReading
 from accounts.models import Trainee
+from django_hstore import hstore
+import json
 import random
 
+def randomsequence():
+    
+    seq = ""
+    for i in range(0, 7):
+        seq += random.choice(['C', 'M', 'N'])
 
-def set_readingstatus():
-    # print random.choice(['C', 'M', 'N'])
-    readingstatus = {}
-    for i in range(0, 19):
-        stat = ""
-        
-        for k in range(0, 7):
-            stat = stat + (random.choice(['C', 'M', 'N']))
-        
-        readstat = {}
-        readstat["status"] = stat
-        readstat["finalized"] = "Y"
-        readingstatus["15_%i" % i] = readstat
+    return seq
 
+def randombooks():
+    books = range(2)
+    books[0] = random.sample(range(0, 65), random.randint(0, 64))
+    books[1] = random.sample(range(39, 65), random.randint(0, 26))
+    s = {}
+    for i in books[0]:
+        s.update({"1_"+str(i): "Y"})
 
-    print str(readingstatus)
-    return {"15_17": "{\"status\": \"CCMNMNM\", \"finalized\": \"Y\"}", "15_18": "{\"status\": \"CCMNMNM\", \"finalized\": \"N\"}"}
+    for i in books[1]:
+        s.update({"2_"+str(i): "Y"})
+
+    return s
 
 def set_booksread():
     return {"1_46": "Y", "1_47": "Y", "1_40": "Y", "1_51": "Y", "1_42": "Y", "1_50": "Y", "1_2": "Y", "2_41": "Y", "2_40": "Y", "2_42": "Y", "2_39": "Y"}
@@ -31,11 +35,19 @@ class Command(BaseCommand):
 # to use: python ap/manage.py populate_trainees --settings=ap.settings.dev
     def _create_biblereading(self): 
 
+        BibleReading.objects.all().delete()
+
         trainees = Trainee.objects.all()
-
         for t in trainees:           
-            BibleReading(trainee=t, weekly_reading_status=set_readingstatus(), books_read=set_booksread()).save()
+            
+            week = {}
+            for i in range(0, 16):
+                k = "{\"status\": \"%s\", \"finalized\": \"Y\"}" % randomsequence()
+                week.update({"15_" + str(i) : str(k)})
+                # week = "15_" + str(i)
+                # print week
 
+            BRrecord = BibleReading.objects.create(trainee=t, weekly_reading_status=week, books_read=randombooks())
 
     def handle(self, *args, **options):
         print('* Creating BibleReading records...')
