a48d788dea691f3d02e6949d245435c314eb8ab4
iamsitting <cdsalamanca27@gmail.cok>
2017-07-03 11:30:23 -0700

    Added inline DatePicker and TimeSelect

--- ap/room_reservations/forms.py
+++ ap/room_reservations/forms.py
@@ -8,9 +8,10 @@ class RoomReservationForm(forms.ModelForm):
         super(RoomReservationForm, self).__init__(*args, **kwargs)
         self.fields['start'].widget = SelectTimeWidget(twelve_hr=True, use_seconds=False, minute_step=30, required=True)
         self.fields['end'].widget = SelectTimeWidget(twelve_hr=True, use_seconds=False, minute_step=30, required=True)
+        # self.fields['date'] = forms.DateField(widget=DatePicker())
     class Meta:
         model = RoomReservation
         fields = ['group', 'date', 'start', 'end', 'room', 'group_size', 'frequency', 'reason']
         widgets = {
-            'date': forms.DateInput(attrs={'class':'datepicker'}),
+            'date': DatePicker(),
             }
