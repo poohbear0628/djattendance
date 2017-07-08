28c0fbb2c572fbd708e4762d14ef53f5c222222b
samchai <sam.chai0501@gmail.com>
2017-07-08 13:52:12 -0700

    fix empty selected object

--- ap/accounts/widgets.py
+++ ap/accounts/widgets.py
@@ -5,7 +5,8 @@ from ap.forms import TraineeSelectForm, EventSelectForm
 
 class TraineeSelect2MultipleInput(Select2MultipleWidget):
   def render(self, name, value, attrs=None):
-    output = super(TraineeSelect2MultipleInput, self).render(name, value, attrs)
+    output = super(TraineeSelect2MultipleInput, self).render(name, attrs)
+    print super(TraineeSelect2MultipleInput, self)
     button = '<button type="button" class="btn btn-default" data-toggle="modal" data-target="#trainee_select"><span class="hide-if-in-class">Add Trainee Group</span></button>'
     select_modal = render_to_string('popups/trainee_select_modal.html', {'trainee_select_form': TraineeSelectForm()})
     return output + mark_safe(button) + mark_safe(select_modal)
@@ -17,7 +18,7 @@ class TraineeSelect2MultipleInput(Select2MultipleWidget):
 
 class EventSelect2MultipleInput(Select2MultipleWidget):
   def render(self, name, value, attrs=None):
-    output = super(EventSelect2MultipleInput, self).render(name, value, attrs)
+    output = super(EventSelect2MultipleInput, self).render(name, attrs)
     button = '<button type="button" class="btn btn-default" data-toggle="modal" data-target="#event_select"><span class="hide-if-in-class">Add Event Group</span></button>'
     select_modal = render_to_string('popups/event_select_modal.html', {'event_select_form': EventSelectForm()})
     return output + mark_safe(button) + mark_safe(select_modal)
