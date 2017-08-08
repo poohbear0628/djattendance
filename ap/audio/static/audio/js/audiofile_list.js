da56455d2de3d2344ff476edfef4949f1c664ea5
Bill Yeh <billwyeh@yahoo.com>
2017-08-08 13:40:23 -0700

    move js and css to own files

---
+++ ap/audio/static/audio/js/audiofile_list.js
@@ -0,0 +1,41 @@
+class AudioList {
+  constructor() {
+    this.audioItems = $('.audio-item');
+  }
+}
+
+$(document).ready(() => {
+  $('#week-select')
+  .select2({
+    data: WEEKS,
+    minimumResultsForSearch: -1,
+  })
+  .val(WEEK)
+  .trigger('change')
+  .on('change', (e) => {
+    window.location = AUDIO_LIST_URL.replace(URL_PLACEHOLDER, e.target.value);
+  });
+
+  // List item scripts
+  let player = new AudioPlayer();
+  let audioList = new AudioList();
+  audioList.audioItems.on('click', (e) => {
+    let panel = e.currentTarget;
+    player.playAudio(panel.getAttribute('src'));
+    audioList.audioItems.removeClass('panel-primary');
+    $(panel).removeClass('panel-default');
+    $(panel).addClass('panel-primary');
+  });
+  if (audioList.audioItems.length > 0) {
+    audioList.audioItems[0].click();
+  }
+
+  $('.download-button').click((e) => {
+    $(e.target).parent().parent().parent().parent().find('.panel-body').toggle();
+    e.stopPropagation();
+  });
+
+  $('.download-link').click((e) => {
+    e.stopPropagation();
+  });
+});
