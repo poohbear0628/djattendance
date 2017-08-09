017723472cff3efbcedf18e3c301024b04042f2d
joonjo <hyunjoon.jo.922@gmail.com>
2017-08-09 15:20:42 -0700

    enhance automation structure

--- selenium/automation/samples/demo_old_attendance.py
+++ selenium/automation/samples/demo_old_attendance.py
@@ -14,7 +14,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
-
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 def send_text_by_tag_name(driver, tag_name, value, enter=False):
@@ -30,10 +29,7 @@ def click_element_by_text(driver, text, pose=2):
 	elem.click()
 	time.sleep(pose)
 
-
 driver = webdriver.Firefox()
-#driver = webdriver.Remote(command_executor='http://localhost:4444/wd/hub', desired_capabilities=DesiredCapabilities.FIREFOX)
-
 
 # get Firefox webdriver and load web page
 server = {	"domain" : "http://attendance.ftta.lan/", "username" : "joonjo", "passwd" : "ftta2016" }
@@ -47,11 +43,9 @@ time.sleep(3)
 #wait = WebDriverWait(driver, 10)
 #element = wait.until(EC.element_to_be_clickable((By.ID,'someid')))
 #element = wait.until(EC.presence_of_element_located((By.ID, "addresses")))
-#print element
 
 # get the FTTA Login
 myHeader = driver.find_elements_by_xpath("//*[contains(text(), 'FTTA Login')]")
-#myHeader = driver.find_element_by_id("Header")
 
 # retreive the attributes 
 #attrs = driver.execute_script('var items = {}; for (index = 0; index < arguments[0].attributes.length; ++index) { items[arguments[0].attributes[index].name] = arguments[0].attributes[index].value }; return items;', myHeader)
--- selenium/automation/samples/demo_python_selenium_webdriver.py
+++ selenium/automation/samples/demo_python_selenium_webdriver.py
@@ -9,14 +9,9 @@ from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.common.keys import Keys
 
-#driver = webdriver.Remote(command_executor='http://localhost:4444/wd/hub', desired_capabilities=DesiredCapabilities.FIREFOX)
-#driver.set_window_size(1280,1024)
-
 driver = webdriver.Firefox()
 driver.get("http://www.python.org")
-
-#driver.get("http://attendance.ftta.lan/")
-#help(driver) # allows you to view a list of variables, functions and comments for an imported module/object
+driver.set_window_size(1280,1024)
 
 assert "Python" in driver.title
 elem = driver.find_element_by_name("q")
