a6812db2b139ecd8204a80a368b45a170eb28ee1
Bill Yeh <billwyeh@yahoo.com>
2017-09-13 19:15:03 -0700

    use max_length and allow superclass to do its thing

--- ap/aputils/utils.py
+++ ap/aputils/utils.py
@@ -21,10 +21,10 @@ class OverwriteStorage(FileSystemStorage):
   Removes a duplicate file before storing because otherwise Django will just
   add random letters to the end of the filename.
   """
-  def get_available_name(self, name):
+  def get_available_name(self, name, max_length):
     if self.exists(name):
       os.remove(os.path.join(self.location, name))
-    return name
+    return super(OverwriteStorage, self).get_available_name(name, max_length)
 
 def modify_model_status(model, url):
   @group_required(('administration',), raise_exception=True)
