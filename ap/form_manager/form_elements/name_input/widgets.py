5d793cf4b6a63be32bbea78011655a868bcca177
iamsitting <cdsalamanca27@gmail.com>
2017-09-22 11:30:38 -0700

    Custom Form Element

---
+++ ap/form_manager/form_elements/name_input/widgets.py
@@ -0,0 +1,6 @@
+from fobi.base import FormElementPluginWidget
+
+class NameInputPluginWidget (FormElementPluginWidget):
+  """Base sample textarea plugin widget."""
+  # Same as ``uid`` value of the ``SampleTextareaPlugin``.
+  plugin_uid = "name_input"
