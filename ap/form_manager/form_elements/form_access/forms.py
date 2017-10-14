f2f2050dbfdc1cca2618cd13582dbd9bd9e6d0c7
iamsitting <cdsalamanca27@gmail.com>
2017-10-14 15:50:01 -0700

    Trainee choices in init

--- ap/form_manager/form_elements/form_access/forms.py
+++ ap/form_manager/form_elements/form_access/forms.py
@@ -14,16 +14,28 @@ class FormAccessForm(forms.Form, BasePluginForm):
     ("required", False)
   ]
 
-  TRAINEE_CHOICES = ((-1, 'all'), )
-  trainee_qs = Trainee.objects.all()
-  for t in trainee_qs:
-    TRAINEE_CHOICES += ((t.id, t.full_name), )
+  def __init__(self):
+    super(FormAccessForm, self).__init__()
+    TRAINEE_CHOICES = ((-1, 'all'), )
+    trainee_qs = Trainee.objects.all()
+    for t in trainee_qs:
+      TRAINEE_CHOICES += ((t.id, t.full_name), )
+    
+    # self.TRAINEE_CHOICES = TRAINEE_CHOICES
+    self.fields['initial'] = forms.CharField(
+      label="Initial",
+      required=False,
+      widget=forms.SelectMultiple(choices=TRAINEE_CHOICES)
+    )
+
+  def get_choices(self):
+    return self.TRAINEE_CHOICES
 
   name = forms.CharField(label="Name", required=True)
   label = forms.CharField(label="Label", required=True)
-  initial = forms.CharField(
-    label="Initial",
-    required=False,
-    widget=forms.SelectMultiple(choices=TRAINEE_CHOICES)
-  )
+  # initial = forms.CharField(
+  #  label="Initial",
+  #  required=False,
+  #  widget=forms.SelectMultiple(choices=get_choices())
+  # )
   required = forms.BooleanField(label="Required", required=False)
