69c024837d2e52fb2c2ef31ad8aeb3aacbb312e6
ruthnan <ruthnan@gmail.com>
2017-10-29 18:44:26 -0700

    fixed the HC permissions for HC recommendations and HC surveys

--- ap/hc/urls.py
+++ ap/hc/urls.py
@@ -1,6 +1,8 @@
 from hc import views
 
+app_name = 'hc'
+
 urlpatterns = [
-  url(r'^hc_survey/$', views.create_hc_survey, name='hc-survey'),
-  url(r'^hc_recommendation/$', views.HCRecommendationCreate.as_view(), name='hc-recommendation'),
+  url(r'hc_survey/$', views.create_hc_survey, name='hc_survey'),
+  url(r'hc_recommendation/$', views.HCRecommendationCreate.as_view(), name='hc_recommendation'),
 ]
