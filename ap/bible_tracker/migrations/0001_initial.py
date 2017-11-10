0c463eac82b5abb2c0c78556ae3c227280b13318
Jonathan Tey <jontey88@gmail.com>
2017-11-09 18:13:48 -0800

    Default HstoreExtension first

--- ap/bible_tracker/migrations/0001_initial.py
+++ ap/bible_tracker/migrations/0001_initial.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import models, migrations
-import django.contrib.postgres.fields.hstore
+from django.db import migrations
 from django.contrib.postgres.operations import HStoreExtension
 
 
@@ -14,12 +13,4 @@ class Migration(migrations.Migration):
 
     operations = [
         HStoreExtension(),
-        migrations.CreateModel(
-            name='BibleReading',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('weekly_reading_status', django.contrib.postgres.fields.hstore.HStoreField()),
-                ('books_read', django.contrib.postgres.fields.hstore.HStoreField()),
-            ],
-        ),
     ]
