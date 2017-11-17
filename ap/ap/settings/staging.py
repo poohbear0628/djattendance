2285d470e872b3c2bc88ae6749a3e4f6b39f43c0
Bill Yeh <billwyeh@yahoo.com>
2017-11-16 18:34:53 -0800

    whitespace

--- ap/ap/settings/staging.py
+++ ap/ap/settings/staging.py
@@ -6,7 +6,7 @@ import os
 DEBUG = True
 
 # Parse database configuration from $DATABASE_URL
-DATABASES['default'] =  dj_database_url.config()
+DATABASES['default'] = dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
@@ -15,11 +15,11 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS += (
-  'debug_toolbar',
+    'debug_toolbar',
 )
 
 DEBUG_TOOLBAR_CONFIG = {
-  'JQUERY_URL': '',  # use local jquery (for offline development)
+    'JQUERY_URL': '',  # use local jquery (for offline development)
 }
 
 # Static asset configuration
@@ -31,7 +31,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-  os.path.join(BASE_DIR, 'ap/static'),
+    os.path.join(BASE_DIR, 'ap/static'),
 )
 
 # Simplified static file serving.
@@ -39,12 +39,12 @@ STATICFILES_DIRS = (
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 WEBPACK_LOADER = {
-  'DEFAULT': {
-    'BUNDLE_DIR_NAME': 'bundles/',
-    'STATS_FILE': os.path.join(BASE_DIR, 'webpack/webpack-stats.json'),
-  }
+    'DEFAULT': {
+        'BUNDLE_DIR_NAME': 'bundles/',
+        'STATS_FILE': os.path.join(BASE_DIR, 'webpack/webpack-stats.json'),
+    }
 }
 
 MIDDLEWARE += (
-  'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
