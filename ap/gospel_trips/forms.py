4155113fe6bf95cbd8236430ea68d04590d56824
iamsitting <cdsalamanca27@gmail.com>
2017-12-20 13:36:29 -0800

    modularizing big function

--- ap/gospel_trips/forms.py
+++ ap/gospel_trips/forms.py
@@ -70,3 +70,85 @@ class AnswerForm(forms.ModelForm):
   class Meta:
     model = Answer
     fields = ['response', ]
+
+
+def form_from_object(obj):
+  FORM_FROM_OBJECT = {
+    'GospelTrip': GospelTripForm,
+    'Section': SectionForm,
+    'Instruction': InstructionForm,
+    'Question': QuestionForm
+  }
+  return FORM_FROM_OBJECT[obj.__class__.__name__]
+
+
+def build_form(obj, data={}, prefix=None):
+  # prefix - a unique identifier for form data; also tracks order
+  if data:
+    return form_from_object[obj](data, instance=obj, prefix=prefix)
+  else:
+    return form_from_object[obj](instance=obj, prefix=prefix)
+
+
+def gospel_trip_forms(gospel_trip, data={}):
+  # returns a list of forms
+  # gospel_trip - an existing GospelTrip object
+  # data - a dictionary containing HTTP Request POST data
+
+  new_s = False  # if data contains new section form data
+  new_i = False  # if data contains new instruction form data
+  new_q = False  # if data contains new question form data
+
+  all_forms = [build_form(gospel_trip, data)]
+
+  sections = Section.objects.filter(gospel_trip=gospel_trip)
+  for s in sections:
+
+    prefix = 's' + str(s.index)
+    all_forms.append(build_form(s, data, prefix))
+
+    instructions = Instruction.objects.filter(section=s)
+    for i in instructions:
+      prefix = 'i' + str(i.index)
+      all_forms.append(build_form(i, data, prefix))
+
+    else:
+      if not new_i:
+        prefix = 'i' + str(len(instructions) + 1)
+        all_forms.append(InstructionForm(data, prefix=prefix))
+        new_i = True
+
+    if not data:
+      if not new_i:
+        prefix = 'i' + str(len(instructions) + 1)
+        all_forms.append(InstructionForm(prefix=prefix, instance=Instruction()))
+        new_i = True
+
+    questions = Question.objects.filter(section=s)
+    for q in questions:
+
+      prefix = 'q' + str(q.index)
+      all_forms.append(build_form(q, data, prefix))
+
+    else:
+      prefix = 'q' + str(len(questions) + 1)
+      if not new_q:
+        all_forms.append(QuestionForm(data, prefix=prefix))
+        new_q = True
+    if not data:
+      if not new_q:
+        prefix = 'q' + str(len(questions) + 1)
+        all_forms.append(QuestionForm(prefix=prefix, instance=Question()))
+        new_q = True
+  else:
+    if not new_s:
+      prefix = 's' + str(len(sections) + 1)
+      all_forms.append(SectionForm(data, prefix=prefix))
+      new_s = True
+
+  if not data:
+    if not new_s:
+      prefix = 's' + str(len(sections) + 1)
+      all_forms.append(SectionForm(prefix=prefix, instance=Section()))
+      new_s = True
+  return all_forms
--- ap/gospel_trips/views.py
+++ ap/gospel_trips/views.py
@@ -5,69 +5,8 @@ from django.shortcuts import render
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 
-from gospel_trips.models import GospelTrip, Section, Instruction, Question, Answer
-from gospel_trips.forms import GospelTripForm, SectionForm, InstructionForm, QuestionForm, AnswerForm
-
-
-def gospel_trip_forms(gospel_trip, data={}):
-  new_s = False
-  new_i = False
-  new_q = False
-
-  if data:
-    all_forms = [GospelTripForm(data, instance=gospel_trip)]
-  else:
-    all_forms = [GospelTripForm(instance=gospel_trip)]
-
-  sections = Section.objects.filter(gospel_trip=gospel_trip)
-  for s in sections:
-
-    if data:
-      all_forms.append(SectionForm(data, prefix='s' + str(s.index), instance=s))
-    else:
-      all_forms.append(SectionForm(prefix='s' + str(s.index), instance=s))
-
-    instructions = Instruction.objects.filter(section=s)
-    for i in instructions:
-      if data:
-        all_forms.append(InstructionForm(data, prefix='i' + str(i.index), instance=i))
-      else:
-        all_forms.append(InstructionForm(prefix='i' + str(i.index), instance=i))
-    else:
-      if not new_i:
-        all_forms.append(InstructionForm(data, prefix='i' + str(len(instructions) + 1)))
-        new_i = True
-
-    if not data:
-      if not new_i:
-        all_forms.append(InstructionForm(prefix='i' + str(len(instructions) + 1), instance=Instruction()))
-        new_i = True
-
-    questions = Question.objects.filter(section=s)
-    for q in questions:
-      if data:
-        all_forms.append(QuestionForm(data, prefix='q' + str(q.index), instance=q))
-      else:
-        all_forms.append(QuestionForm(prefix='q' + str(q.index), instance=q))
-    else:
-      if not new_q:
-        all_forms.append(QuestionForm(data, prefix='q' + str(len(questions) + 1)))
-        new_q = True
-    if not data:
-      if not new_q:
-        all_forms.append(QuestionForm(prefix='q' + str(len(questions) + 1), instance=Question()))
-        new_q = True
-
-  else:
-    if not new_s:
-      all_forms.append(SectionForm(data, prefix='s' + str(len(sections) + 1)))
-      new_s = True
-
-  if not data:
-    if not new_s:
-      all_forms.append(SectionForm(prefix='s' + str(len(sections) + 1), instance=Section()))
-      new_s = True
-  return all_forms
+from gospel_trips.models import GospelTrip
+from gospel_trips.forms import gospel_trip_forms
 
 
 # Create your views here.
