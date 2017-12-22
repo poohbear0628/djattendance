3c5f1aa2c774d137c18c048aa604570d59e960d3
berlyseria <berlyseria@hotmail.com>
2017-12-21 16:59:38 -0800

    Commit to merge

--- ap/ap/views.py
+++ ap/ap/views.py
@@ -10,26 +10,8 @@ from aputils.trainee_utils import is_trainee, is_TA, trainee_from_user
 from bible_tracker.models import BibleReading
 
 @login_required
-
-def calcFirstYearProgress(user_checked_list):
-    first_year_checked_list = [int(book_code.split("_")[1]) for book_code in user_checked_list.keys() if book_code.startswith('1_')]
-    first_year_progress = 0
-
-    for checked_book in first_year_checked_list:
-        first_year_progress = first_year_progress + sum([int(chapter_verse_count) for chapter_verse_count in bible_books[checked_book][3]])
-
-    return (first_year_checked_list, int(float(first_year_progress) / 31102.0 * 100))
-
-def calcSecondYearProgress(user_checked_list):
-    second_year_checked_list = [int(book_code.split("_")[1]) for book_code in user_checked_list.keys() if book_code.startswith('2_')]
-    second_year_progress = 0
-
-    for checked_book in second_year_checked_list:
-        second_year_progress = second_year_progress + sum([int(chapter_verse_count) for chapter_verse_count in bible_books[checked_book][3]])
-
-    return (second_year_checked_list, int(float(second_year_progress) / 7957.0 * 100))
-
 def home(request):
+
   data = {
     'daily_nourishment': Portion.today(),
     'user': request.user,
@@ -42,15 +24,24 @@ def home(request):
 
   data['popups'] = get_popups(request)
 
+  print "hello first"
+
   if is_trainee(request.user):
     trainee = trainee_from_user(request.user)
     data['schedules'] = trainee.active_schedules
 
     trainee_bible_reading = BibleReading.objects.get(trainee=trainee)
+
+    print "hello"
+    print trainee_bible_reading
+
     user_checked_list = trainee_bible_reading.books_read
 
-    data['first_year_progress'] = calcFirstYearProgress(user_checked_list)
-    data['second_year_progress'] = calcSecondYearProgress(user_checked_list)
+    first_year_checked_list, first_year_progress = calcFirstYearProgress(user_checked_list)
+    second_year_checked_list, second_year_progress = calcSecondYearProgress(user_checked_list)
+
+    data['first_year_progress'] = first_year_progress
+    data['second_year_progress'] = second_year_progress
 
   elif is_TA(request.user):
     #do stuff to TA
