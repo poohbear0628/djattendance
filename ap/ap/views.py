3126350a9ed0c29275ba503336864174dba947db
berlyseria <berlyseria@hotmail.com>
2017-12-21 16:12:06 -0800

    changed views.py for progress bar

--- ap/ap/views.py
+++ ap/ap/views.py
@@ -10,6 +10,25 @@ from aputils.trainee_utils import is_trainee, is_TA, trainee_from_user
 from bible_tracker.models import BibleReading
 
 @login_required
+
+def calcFirstYearProgress(user_checked_list):
+    first_year_checked_list = [int(book_code.split("_")[1]) for book_code in user_checked_list.keys() if book_code.startswith('1_')]
+    first_year_progress = 0
+
+    for checked_book in first_year_checked_list:
+        first_year_progress = first_year_progress + sum([int(chapter_verse_count) for chapter_verse_count in bible_books[checked_book][3]])
+
+    return (first_year_checked_list, int(float(first_year_progress) / 31102.0 * 100))
+
+def calcSecondYearProgress(user_checked_list):
+    second_year_checked_list = [int(book_code.split("_")[1]) for book_code in user_checked_list.keys() if book_code.startswith('2_')]
+    second_year_progress = 0
+
+    for checked_book in second_year_checked_list:
+        second_year_progress = second_year_progress + sum([int(chapter_verse_count) for chapter_verse_count in bible_books[checked_book][3]])
+
+    return (second_year_checked_list, int(float(second_year_progress) / 7957.0 * 100))
+
 def home(request):
   data = {
     'daily_nourishment': Portion.today(),
@@ -30,8 +49,8 @@ def home(request):
     trainee_bible_reading = BibleReading.objects.get(trainee=trainee)
     user_checked_list = trainee_bible_reading.books_read
 
-    data['first_year_progress'] = BibleReading.calcFirstYearProgress(user_checked_list)
-    data['second_year_progress'] = BibleReading.calcSecondYearProgress(user_checked_list)
+    data['first_year_progress'] = calcFirstYearProgress(user_checked_list)
+    data['second_year_progress'] = calcSecondYearProgress(user_checked_list)
 
   elif is_TA(request.user):
     #do stuff to TA
