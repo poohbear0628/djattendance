f46b8b13489fc61aeedd803b0ba82cb3881817c1
Benjamin Liu <yipingbenjamin.l@gmail.com>
2018-02-15 18:10:39 -0800

    Landing page for TA and trainee
    * hardcoded cards html + css added to index.html
    
    * made the css for the panels and created panels in the bible tracker page
    
    * cleaned up unused css from previous commit
    
    * moved panels from bible_tracker page to homepage
    
    * added rightside pending numbers
    
    * convert tabs to 2 spaces
    
    * weekly summary update
    
    * panels are dynamically generated from javascript list
    
    * made panels dynamic
    
    * weekly bible tracker dropdown
    
    * fix up js a little bit. still needs further work.
    
    * minor line cleanup
    
    * adding smart_cards.py to generate cards for homepage
    
    * comment out daily nourishment
    
    * card-link url works
    
    * initial commit
    
    * card permissions working and more flexible!
    
    * cleaned code a tiny bit.
    
    * progress bar update
    
    * changed views.py for progress bar
    
    * Commit to merge
    
    * testing prog_bar
    
    * Fix bible reading front page
    
    * progress bar progress.. with errors
    
    * still fixing, almost done
    
    * progress bar working
    
    * added package-lock.json
    
    * Refactor
    
    * clear progress bar code a bit
    
    * set 5px from header to bar, 10px bar to list
    
    * Fix typography
    Welcome text should be H1 lol
    
    * progress bar done. just working on css
    
    * remove file
    
    * Bible reading dropdown (#688)
    
    * dropdown selected text replaces dropdown label
    
    * Made some fields dynamic
    
    * Removed javascript stuff. Panels are generated using panels.py template tag.
    
    * trainee homepage html organization. Some CSS for table. Still working on CSS for dropdowns (week select and bible select).
    
    * remove empty spaces and comments
    
    * quick fix for progress bar
    
    * refactor with django template tags
    
    * added cards to homepage
    
    * fitting cards into container
    
    * panel links now dynamic
    
    * panel links now dynamic
    
    * minor fix
    
    * panels are now dynamic
    
    * bible reading progress bar now leads to bible tracker
    
    * make trainee landing page robust for development
    
    * ho! improving cards
    
    * changed wkly sum table to bootstrap grid
    
    * starting css for weekly sum
    
    * working on search bar for TAs/AMs
    
    * trainee search css - hacky box
    
    * fixed panel numbering bugs.
    
    * permissions added to distinguish between ta and trainee
    
    * Fix trainee search box for TA on Firefox
    Add margins to cards container to better reflect design mockup
    
    * Whitespace
    
    * commented out trainee landing page that's yet to be complete

--- ap/ap/views.py
+++ ap/ap/views.py
@@ -1,17 +1,23 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from datetime import date
 from dailybread.models import Portion
 from announcements.notifications import get_announcements, get_popups
 
-
 from aputils.trainee_utils import is_trainee, is_TA, trainee_from_user
+from bible_tracker.models import BibleReading
+from terms.models import Term
 
 @login_required
 def home(request):
+
   data = {
     'daily_nourishment': Portion.today(),
-    'user': request.user
+    'user': request.user,
+    'trainee_info': BibleReading.weekly_statistics,
+    'current_week' : Term.current_term().term_week_of_date(date.today()),
+    'weeks' : Term.all_weeks_choices()
   }
 
   notifications = get_announcements(request)
@@ -24,6 +30,16 @@ def home(request):
   if is_trainee(request.user):
     trainee = trainee_from_user(request.user)
     data['schedules'] = trainee.active_schedules
+
+    # Bible Reading progress bar
+    trainee_bible_reading = BibleReading.objects.filter(trainee=trainee).first()
+
+    if (trainee_bible_reading == None):
+      data['bible_reading_progress'] = 0
+    else:
+      year_checked_list, year_progress = BibleReading.calcBibleReadingProgress(trainee_bible_reading, request.user)
+      data['bible_reading_progress'] = year_progress
+
   elif is_TA(request.user):
     #do stuff to TA
     pass
