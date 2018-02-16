f196a47939f6c142e538aa63500172330729c015
Bill Yeh <billwyeh@yahoo.com>
2018-02-16 11:52:29 -0800

    roll forms with multiple events/trainees, submit doesn't work yet

--- ap/attendance/admin.py
+++ ap/attendance/admin.py
@@ -1,56 +1,51 @@
 from django import forms
 from django.contrib import admin
-
-from django_select2.forms import ModelSelect2Widget
+from django_select2.forms import ModelSelect2Widget, ModelSelect2MultipleWidget
 
 from .models import Roll, Trainee, Event
+from accounts.widgets import TraineeSelect2MultipleInput
 
 
 class RollAdminForm(forms.ModelForm):
-  event = forms.ModelChoiceField(
+  class Meta:
+    fields = ['event', 'trainee', 'status', 'finalized', 'date', 'notes', 'submitted_by']
+    model = Roll
+
+  event = forms.ModelMultipleChoiceField(
       label="Event",
       queryset=Event.objects.all(),
       required=True,
-      widget=ModelSelect2Widget(
+      widget=ModelSelect2MultipleWidget(
           queryset=Event.objects.all(),
           required=True,
           search_fields=['name__icontains', 'weekday__icontains'],
       )
   )
 
-  trainee = forms.ModelChoiceField(
-      label="Trainee",
+  trainee = forms.ModelMultipleChoiceField(
       queryset=Trainee.objects.all(),
-      required=True,
-      widget=ModelSelect2Widget(
-          queryset=Trainee.objects.all(),
-          required=True,
-          search_fields=['firstname__icontains', 'lastname__icontains'],
-      )
+      label='Trainee(s)',
+      required=False,
+      widget=TraineeSelect2MultipleInput(attrs={'id': 'id_trainees'}),
   )
 
   submitted_by = forms.ModelChoiceField(
-      label="Submitted by",
+      label="",
       queryset=Trainee.objects.filter(is_active=True),
-      required=True,
-      widget=ModelSelect2Widget(
-          queryset=Trainee.objects.filter(is_active=True),
-          required=True,
-          search_fields=['firstname__icontains', 'lastname__icontains'],
-      )
+      required=False,
+      widget=forms.HiddenInput,
   )
 
-  def __init__(self, *args, **kwargs):
-    self.user = kwargs.pop('user', None)
-    super(RollAdminForm, self).__init__(*args, **kwargs)
-    self.initial['Submitted by'] = self.user
+  def save(self, commit=True):
+    print(self.cleaned_data)
+    return 
 
 
 class RollAdmin(admin.ModelAdmin):
-  list_display = ('date', 'event', 'trainee', 'status', 'finalized')
+  list_display = ('date', 'event', 'status', 'finalized')
   list_filter = ('date', 'event__name', 'status')
-  ordering = ('date', 'event', 'trainee')
-  search_fields = ('trainee__firstname', 'trainee__lastname', 'event__name', 'status', 'date')
+  ordering = ('date', 'event')
+  search_fields = ('trainee__firstname', 'trainee__lastname', 'event__name', 'event__weekday', 'status', 'date')
   form = RollAdminForm
 
 
