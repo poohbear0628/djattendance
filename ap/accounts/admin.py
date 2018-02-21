de75d90e3bf86e38405b6a18130a93e9a241048d
Bill Yeh <billwyeh@yahoo.com>
2018-02-20 18:26:44 -0800

    add gender to trainee list filter (#747)

--- ap/accounts/admin.py
+++ ap/accounts/admin.py
@@ -210,7 +210,7 @@ class TraineeAdmin(ForeignKeyAutocompleteAdmin, UserAdmin):
 
   # TODO(useropt): removed bunk, married, and spouse
   list_display = ('full_name', 'current_term', 'email', 'team', 'house',)
-  list_filter = (CurrentTermListFilter, FirstTermMentorListFilter,)
+  list_filter = (CurrentTermListFilter, FirstTermMentorListFilter, 'gender',)
 
   ordering = ('firstname', 'lastname',)
   filter_horizontal = ("groups", "user_permissions")
