2b5308cb3669fedb1475230ef86b833e99fdf0a9
iamsitting <cdsalamanca27@gmail.com>
2018-02-26 14:22:21 -0800

    forced only on wednesdays now

--- ap/bible_tracker/utils.py
+++ ap/bible_tracker/utils.py
@@ -14,19 +14,20 @@ def bible_reading_finalized(user):
       today = datetime.now().date()
       week = term.term_week_of_date(today)
       if week > 0:
-        week = week - 1
+        week = week - 1  # previous week
       term_week_code = str(term.id) + "_" + str(week)
 
       try:
         trainee_weekly_reading = BibleReading.objects.get(trainee=trainee).weekly_reading_status[term_week_code]
-        json_weekly_reading = json.dumps(trainee_weekly_reading)
+        json_weekly_reading = json.loads(trainee_weekly_reading)
       except (BibleReading.DoesNotExist, KeyError):
         trainee_weekly_reading = "{\"status\": \"_______\", \"finalized\": \"N\"}"
         json_weekly_reading = json.loads(trainee_weekly_reading)
 
       try:
-        if str(json_weekly_reading['finalized']) == 'N' and (today > get_prev_tuesday()):
-          finalized = False
+        if today > term.startdate_of_week(week) + timedelta(8):
+          if str(json_weekly_reading['finalized']) == 'N':
+            finalized = False
       except TypeError:
         pass
 
@@ -34,9 +35,3 @@ def bible_reading_finalized(user):
     pass
 
   return finalized
-
-
-def get_prev_tuesday():
-  today = datetime.now().date()
-  t = timedelta((1 - today.weekday()) % 7)  # Mon-0, Sun-6
-  return today - t
