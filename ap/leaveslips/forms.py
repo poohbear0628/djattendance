e4e8970ef541139e0f985ec2349279abed12e7d4
Benjamin Liu <yipingbenjamin.l@gmail.com>
2018-02-26 02:53:02 -0800

    slight UI change for leaveslips view for TA

--- ap/leaveslips/forms.py
+++ ap/leaveslips/forms.py
@@ -12,9 +12,9 @@ class LeaveslipForm(forms.ModelForm):
   def __init__(self, *args, **kwargs):
     super(LeaveslipForm, self).__init__(*args, **kwargs)
     self.fields['description'].widget.attrs['readonly'] = True
-    self.fields['informed'].widget.attrs['onclick'] = 'return false;'
-    self.fields['texted'].widget.attrs['onclick'] = 'return false;'
-    self.fields['TA'].label = 'TA informed'
+    # self.fields['informed'].widget.attrs['onclick'] = 'return false;'
+    # self.fields['texted'].widget.attrs['onclick'] = 'return false;'
+    self.fields['TA'].label = 'TA Assigned to this leave slip'
 
 class IndividualSlipForm(LeaveslipForm):
   def __init__(self, *args, **kwargs):
@@ -23,7 +23,7 @@ class IndividualSlipForm(LeaveslipForm):
 
   class Meta:
     model = IndividualSlip
-    fields = ['trainee', 'type', 'description', 'private_TA_comments', 'comments', 'texted', 'informed', 'TA']
+    fields = ['trainee', 'type', 'description', 'private_TA_comments', 'comments', 'TA']
     widgets = {
       'description': AutosizedTextarea,
       'comments': AutosizedTextarea,
@@ -46,7 +46,7 @@ class GroupSlipForm(forms.ModelForm):
 
   class Meta:
     model = GroupSlip
-    fields = ['trainees', 'type', 'description', 'private_TA_comments', 'comments', 'texted', 'informed', 'start', 'end', 'TA']
+    fields = ['trainees', 'type', 'description', 'private_TA_comments', 'comments', 'start', 'end']
     widgets = {
       'description': AutosizedTextarea,
       'comments': AutosizedTextarea,
