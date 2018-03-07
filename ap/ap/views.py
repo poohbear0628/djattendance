4c788038d9d6c573930512f403580ee3ae532dfa
Bill Yeh <billwyeh@yahoo.com>
2018-03-06 18:34:49 -0800

    whitespace etc.

--- ap/ap/views.py
+++ ap/ap/views.py
@@ -10,50 +10,40 @@ from bible_tracker.models import BibleReading
 from terms.models import Term
 from house_requests.models import MaintenanceRequest
 
+
 @login_required
 def home(request):
-
   user = request.user
-
   data = {
-    'daily_nourishment': Portion.today(),
-    'user': user,
-    'trainee_info': BibleReading.weekly_statistics,
-    'current_week' : Term.current_term().term_week_of_date(date.today()),
-    'weeks' : Term.all_weeks_choices()
+      'daily_nourishment': Portion.today(),
+      'user': user,
+      'trainee_info': BibleReading.weekly_statistics,
+      'current_week': Term.current_term().term_week_of_date(date.today()),
+      'weeks': Term.all_weeks_choices()
   }
-
   notifications = get_announcements(request)
   for notification in notifications:
     tag, content = notification
     messages.add_message(request, tag, content)
-
   data['popups'] = get_popups(request)
 
   if is_trainee(user):
     trainee = trainee_from_user(user)
-    data['schedules'] = trainee.active_schedules
-
     # Bible Reading progress bar
     trainee_bible_reading = BibleReading.objects.filter(trainee=trainee).first()
-
-    if (trainee_bible_reading == None):
+    if trainee_bible_reading is None:
       data['bible_reading_progress'] = 0
     else:
       year_checked_list, year_progress = BibleReading.calcBibleReadingProgress(trainee_bible_reading, user)
       data['bible_reading_progress'] = year_progress
 
-
-  #condition for maintenance brothers
+  # condition for maintenance brothers
   elif is_TA(user) and user.has_group(['facility_maintenance']) and user.groups.all().count() == 1:
     data['house_requests'] = MaintenanceRequest.objects.all()
-    data['request_status'] =[('C', 'Completed'),
-      ('P', 'Pending'),
-      ('F', 'Marked for Fellowship')]
-    #do stuff to TA
-    pass
-  else:
-    #do stuff to other kinds of users
-    pass
+    data['request_status'] = [
+        ('C', 'Completed'),
+        ('P', 'Pending'),
+        ('F', 'Marked for Fellowship')
+    ]
 
   return render(request, 'index.html', context=data)
