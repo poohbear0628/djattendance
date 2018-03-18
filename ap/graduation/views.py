9fe7c17f2314c3512359346a7d04f1a91844c280
Benjamin Liu <yipingbenjamin.l@gmail.com>
2018-03-17 17:32:41 -0700

    Graduation Miscillaneous Report Available (#873)
    * miscillaneous report producing
    
    * link avaiable via lable

--- ap/graduation/views.py
+++ ap/graduation/views.py
@@ -2,10 +2,12 @@
 from __future__ import unicode_literals
 
 from django.views.generic.edit import UpdateView
+from django.views.generic import ListView
+from django.db.models import Sum
 
 from terms.models import Term
-from graduation.models import Testimony, Consideration, Website, Outline, Misc, GradAdmin
-from graduation.forms import TestimonyForm, ConsiderationForm, WebsiteForm, MiscForm, OutlineForm, GradAdminForm
+from graduation.models import *
+from graduation.forms import *
 
 from braces.views import GroupRequiredMixin
 
@@ -102,3 +104,24 @@ class GradAdminView(UpdateView, GroupRequiredMixin):
     ctx['page_title'] = "Grad Admin"
     ctx['button_label'] = 'Save'
     return ctx
+
+class MiscReport(ListView):
+  model = Misc
+  template_name = 'graduation/misc_report.html'
+
+  def get_context_data(self, **kwargs):
+    context = super(MiscReport, self).get_context_data(**kwargs)
+    
+    ct = Term.objects.filter(current=True).first()
+    ga = GradAdmin.objects.get(term=ct)
+    miscellaneous = Misc.objects.filter(grad_admin=ga, trainee__in=Trainee.objects.filter(current_term=4))
+    m = [m for i in miscellaneous if i.responded]
+    
+    context = {
+      'invite_count': miscellaneous.aggregate(Sum('grad_invitations')),
+      'dvd_count': miscellaneous.aggregate(Sum('grad_dvd')),
+      'remembrances': m,
+
+    }
+    return context
+
