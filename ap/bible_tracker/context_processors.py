80d50bdca0dac4af55b85c64c01c7b32e4364508
Bill Yeh <billwyeh@yahoo.com>
2018-04-05 15:57:37 -0700

    simplify bible tracker logic

--- ap/bible_tracker/context_processors.py
+++ ap/bible_tracker/context_processors.py
@@ -2,5 +2,5 @@ from .utils import unfinalized_week
 
 
 def bible_tracker_forced(request):
-    forced = True if unfinalized_week(request.user) else False  # forced if bible reading not finalized
-    return {'bible_tracker_forced': forced}
+  forced = True if unfinalized_week(request.user) else False  # forced if bible reading not finalized
+  return {'bible_tracker_forced': forced}
--- ap/bible_tracker/middleware.py
+++ ap/bible_tracker/middleware.py
@@ -5,20 +5,16 @@ from .utils import unfinalized_week
 
 
 class BibleReadingMiddleware(MiddlewareMixin):
-    forced = False
-
-    def process_request(self, request):
-      url_list = [reverse('bible_tracker:index'), reverse('login'), reverse('logout')]
-      exception_list = [
-          reverse('bible_tracker:changeWeek'), reverse('bible_tracker:updateStatus'),
-          reverse('bible_tracker:finalizeStatus'), reverse('bible_tracker:updateBooks')
-      ]
-      if request.path not in url_list:
-        week = unfinalized_week(request.user)
-        if request.user.is_authenticated() and week:
-          self.forced = True
-          if request.path in exception_list:
-            return None
-          else:
-            return HttpResponseRedirect(reverse('bible_tracker:index') + '?week=' + str(week))
+  def process_request(self, request):
+    url_list = [reverse('bible_tracker:index'), reverse('login'), reverse('logout')]
+    exception_list = [
+        reverse('bible_tracker:changeWeek'), reverse('bible_tracker:updateStatus'),
+        reverse('bible_tracker:finalizeStatus'), reverse('bible_tracker:updateBooks')
+    ]
+    if request.path in exception_list:
       return None
+    if request.path not in url_list:
+      week = unfinalized_week(request.user)
+      if week:
+        return HttpResponseRedirect(reverse('bible_tracker:index') + '?week=' + str(week))
+    return None
