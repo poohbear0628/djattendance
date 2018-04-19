49eae4cfbe8fe10493fdaa7b8b6be4b1cc532da2
iamsitting <cdsalamanca27@gmail.com>
2018-04-18 17:33:07 -0700

    datatable-ify house requests

--- ap/house_requests/views.py
+++ ap/house_requests/views.py
@@ -2,13 +2,52 @@ from django.shortcuts import render
 from django.views import generic
 from django.core.urlresolvers import reverse_lazy
 from django.core.serializers import serialize
-from itertools import chain
 
 from aputils.trainee_utils import is_TA
 from aputils.utils import modify_model_status
 from .models import MaintenanceRequest, LinensRequest, FramingRequest
 from .forms import MaintenanceRequestForm, FramingRequestForm
 from houses.models import Room
+from ap.base_datatable_view import BaseDatatableView
+from django.db.models import Q
+import json
+
+
+class HouseGenericJSON(BaseDatatableView):
+  model = None
+  columns = ['id', 'trainee_author', 'date_requested', 'house', 'status', ]
+  order_columns = ['id', 'trainee_author', 'date_requested', 'house', 'status', ]
+  max_display_length = 120
+
+  def filter_queryset(self, qs):
+    search = self.request.GET.get(u'search[value]', None)
+    ret = qs.none()
+    if search:
+      filters = []
+      filters.append(Q(trainee_author__firstname__icontains=search))
+      filters.append(Q(trainee_author__lastname__icontains=search))
+      filters.append(Q(house__name__icontains=search))
+      filters.append(Q(id=search))
+      for f in filters:
+        try:
+          ret = ret | qs.filter(f)
+        except ValueError:
+          continue
+      return ret
+    else:
+      return qs
+
+
+class MaintenanceRequestJSON(HouseGenericJSON):
+  model = MaintenanceRequest
+
+
+class LinensRequestJSON(HouseGenericJSON):
+  model = LinensRequest
+
+
+class FramingRequestJSON(HouseGenericJSON):
+  model = FramingRequest
 
 
 def NewRequestPage(request):
@@ -146,6 +185,8 @@ class LinensRequestDetail(generic.DetailView):
 
 class RequestList(generic.ListView):
   template_name = 'request_list/list.html'
+  DataTableView = None
+  source_url = ''
 
   def get_queryset(self):
     user_has_service = self.request.user.groups.filter(name__in=['facility_maintenance', 'linens', 'frames']).exists()
@@ -160,27 +201,33 @@ class RequestList(generic.ListView):
     context = super(RequestList, self).get_context_data(**kwargs)
     user_has_service = self.request.user.groups.filter(name__in=['facility_maintenance', 'linens', 'frames']).exists()
     if is_TA(self.request.user) or user_has_service:
-      reqs = self.model.objects.none()
-      for status in ['P', 'F', 'C']:
-        reqs = chain(reqs, self.model.objects.filter(status=status).order_by('date_requested'))
-      context['reqs'] = reqs
+      header = self.DataTableView().get_header()
+      context['source_url'] = self.source_url
+      context['header'] = header
+      context['targets_list'] = json.dumps([i for i, v in enumerate(header)])
     return context
 
 
 class MaintenanceRequestList(RequestList):
   model = MaintenanceRequest
+  DataTableView = MaintenanceRequestJSON
   modify_status_url = 'house_requests:maintenance-modify-status'
   ta_comment_url = 'house_requests:maintenance-tacomment'
   template_name = 'maintenance/maintenance_list.html'
+  source_url = reverse_lazy("house_requests:maintenance-json")
 
 
 class LinensRequestList(RequestList):
   model = LinensRequest
+  DataTableView = LinensRequestJSON
   modify_status_url = 'house_requests:linens-modify-status'
   ta_comment_url = 'house_requests:linens-tacomment'
+  source_url = reverse_lazy("house_requests:linens-json")
 
 
 class FramingRequestList(RequestList):
   model = FramingRequest
+  DataTableView = FramingRequestJSON
   modify_status_url = 'house_requests:framing-modify-status'
   ta_comment_url = 'house_requests:framing-tacomment'
+  source_url = reverse_lazy("house_requests:framing-json")
