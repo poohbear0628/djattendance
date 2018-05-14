58350f37f401bf6cf08a2ce5e50aeadcb775b6bc
iamsitting <cdsalamanca27@gmail.com>
2018-05-13 22:10:13 -0700

    codacy, admin.py

--- ap/gospel_trips/admin.py
+++ ap/gospel_trips/admin.py
@@ -2,5 +2,12 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-
+from .models import GospelTrip, Section, Instruction, Question, Answer
 # Register your models here.
+
+admin.site.register(GospelTrip)
+admin.site.register(Section)
+admin.site.register(Instruction)
+admin.site.register(Question)
+admin.site.register(Answer)
+
