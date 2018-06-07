0a5548c48cc42c00746b5a6c09ff3de150eea80d
Benjamin Liu <yipingbenjamin.l@gmail.com>
2018-06-06 17:55:31 -0700

    fix speaking trainee input, hacks

--- ap/graduation/views.py
+++ ap/graduation/views.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.views.generic.edit import UpdateView
 from django.views.generic import ListView
 from django.template.defaultfilters import title
+from django.shortcuts import redirect
 
 from terms.models import Term
 from graduation.models import *
@@ -70,6 +71,14 @@ class OutlineView(CreateUpdateView):
   form_class = OutlineForm
   template_name = 'graduation/outline.html'
 
+  # hack for those speaking, needs to be cleaned up and used the proper methods, override the default clean methods
+  # instead of using a form invalid as a hack
+  def form_invalid(self, form):
+    obj = self.object
+    obj.speaking_section = form.cleaned_data['speaking_section']
+    obj.speaking = form.cleaned_data['speaking']
+    obj.save()
+    return redirect('graduation:outline-view')
 
 class RemembranceView(CreateUpdateView):
   model = Remembrance
