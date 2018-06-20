f9ebf78ada765735f63b586c7e31d675e148dee3
iamsitting <cdsalamanca27@gmail.com>
2018-06-19 18:31:36 -0700

    1154/classnotes (#1181)
    * clean up
    
    * more cleaning
    
    * datatablee for report
    
    * create form (not sure if needed)
    
    * assign by week
    
    * assign by week, report by week
    
    * added url
    
    * small changes
    
    * classnotes single trainee page
    
    * single trainee page improvements
    
    * approve in single trainee viewer
    
    * show status
    
    * toggle received/unreceived
    
    * better page heading
    
    * fixed if statement for no class notes, and ValueError error
    
    * Changed loading gif to match lifestudies
    
    * trainee can edit now by fixing bad python
    
    * improved post redirects
    
    * TAs can use hard copy button
    
    * trainees can submit if hard copy
    
    * Clearer instructions
    
    * adjust submission toggle based upon checkbox
    
    * ;

--- ap/classnotes/admin.py
+++ ap/classnotes/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from .models import Classnotes
 
+
 class ClassnotesAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'trainee','event','date')
+  list_display = ['pk', 'trainee', 'event', 'date']
+
 
 # Register your models here.
 admin.site.register(Classnotes, ClassnotesAdmin)
