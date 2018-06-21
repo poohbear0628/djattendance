c68c3194a9ae0ae389a8f6c6b11da06218e0e073
David Tien <david.h.tien@gmail.com>
2018-06-20 18:41:24 -0700

    code cleanup, style changes

--- ap/room_reservations/static/room_reservations/js/clock.js
+++ ap/room_reservations/static/room_reservations/js/clock.js
@@ -1,22 +1,23 @@
 // taken and edited from http://widget.time.is/en.js
 
+// do not change this variable name, the time.is api relies on it
 var time_is_widget = new time_is_widget();
+
 function time_is_widget() {
-    var ca = 0, tD = 0, tout = 0, updint = 1000, tl = "", U = "undefined", i, j, rd;
+    // variable initialization
+    var i, j;
+    var config = 0, timeDiff = 0, timeout = 0, updint = 1000, U = "undefined";
     var p = {
-        n: ["Sunday.Monday.Tuesday.Wednesday.Thursday.Friday.Saturday.Sun.Mon.Tue.Wed.Thu.Fri.Sat.January.February.March.April.May.June.July.August.September.October.November.December"],
+        n: "Sunday.Monday.Tuesday.Wednesday.Thursday.Friday.Saturday.Sun.Mon.Tue.Wed.Thu.Fri.Sat.January.February.March.April.May.June.July.August.September.October.November.December",
         w: "week ",
         W: "week .n",
         dy: " day"
-    }
-    for (i in p) {
-        p[i] = decodeURIComponent(p[i]);
-        console.log(p[i]);
-    }
+    };
     p["n"] = p["n"].split(".");
+
     this.init = function(config) {
-        if (tout != 0) {
-            clearInterval(time_is_widget.tout);
+        if (timeout != 0) {
+            clearInterval(time_is_widget.timeout);
         }
         var a, q = [], c = {
             dayname: "%l",
@@ -35,11 +36,12 @@ function time_is_widget() {
             minutes: "%i",
             seconds: "%s",
             AMPM: "%A"
-        }
-        var ct = "TIME";
-        var tF = "%H:%i:%s";
-        var dF = "%Y-%d-%m";
-        var sF = "%srH:%srm-%ssH:%ssm";
+        };
+        var template = "TIME";
+        var timeFormat = "%H:%i:%s";
+        var dateFormat = "%Y-%d-%m";
+        var sunFormat = "%srH:%srm-%ssH:%ssm";
+        // this for loop is to allow for multiple times to be displayed based on how many locations you list in the config obj
         for (i in config) {
             console.log("i", i);
             a = config[i];
@@ -49,28 +51,28 @@ function time_is_widget() {
                 a["id"] = i;
             }
             if (typeof a["time_format"] != U) {
-                tF = a["time_format"];
+                timeFormat = a["time_format"];
             }
             if (typeof a["date_format"] != U) {
-                dF = a["date_format"];
+                dateFormat = a["date_format"];
             }
             if (typeof a["sun_format"] != U) {
-                sF = a["sun_format"];
+                sunFormat = a["sun_format"];
             }
+            console.log("c", c);
             for (j in c) {
-                dF = dF.replace(j, c[j]);
-                console.log("dF", dF);
-                tF = tF.replace(j, c[j]);
-                console.log("tF", tF);
+                dateFormat = dateFormat.replace(j, c[j]);
+                timeFormat = timeFormat.replace(j, c[j]);
             }
+            console.log("dateFormat", dateFormat);
+            console.log("timeFormat", timeFormat);
             if (typeof a["template"] != U) {
-                ct = a["template"];
+                template = a["template"];
             }
-            var time_html = '<div id="clock-container" class="row"><div class="col-sm-12"><span id="clock">' + tF + '</span></div></div>';
-            var date_html = '<div id="date-container" class="row"><div class="col-sm-12"><span id="date">' + dF + '</span></div></div>';
-            console.log("tl", tl);
-            a["template"] = ct.replace("SUN", tl + sF + "</span>").replace("TIME", time_html).replace("DATE", date_html);
-            if (typeof a["v"] == U) {
+            var time_html = '<div id="clock-container" class="row"><div class="col-sm-12"><span id="clock">' + timeFormat + '</span></div></div>';
+            var date_html = '<div id="date-container" class="row"><div class="col-sm-12"><span id="date">' + dateFormat + '</span></div></div>';
+            a["template"] = template.replace("SUN", "<span>" + sunFormat + "</span>").replace("TIME", time_html).replace("DATE", date_html);
+            if (typeof a["vector"] == U) {
                 q.push(a["id"]);
                 if (typeof a["coords"] != U) {
                     console.log(8)
@@ -80,8 +82,14 @@ function time_is_widget() {
             config[i] = a;
             console.log("a", a);
         }
-        this.ca = config;
-        if (0 < q.length) {
+        this.config = config;
+        console.log("q", q);
+        if (q.length > 0) {
+            // this is where the magic happens, this is why we can't change the initial variable name
+            // this script tag loads a one line script that looks something like:
+            // time_is_widget.cb(1529542150750,1529542150486,[[-420,1541321999999,-480,0,0,0,0,0,0]])
+            // the parameters passed into cb are most likely from the query parameters passed into the src url which looks something like:
+            // http://widget.time.is/?Anaheim_z14e&t=1529542150486
             i = document.createElement("script");
             i.setAttribute("src", "//widget.time.is/?" + encodeURIComponent(q.join("..")) + "&t=" + new Date().getTime());
             j = document.getElementsByTagName("head").item(0);
@@ -90,34 +98,43 @@ function time_is_widget() {
         } else {
             this.tick();
         }
-    }
+    };
+
     this.cb = function(t, r, a) {
         console.log('cb');
-        var rpT = new Date(), n = 0;
-        time_is_widget.tD = rpT.getTime() - t - Math.round((rpT - r) / 2);
-        for (i in this.ca) {
-            this.ca[i]["v"] = a[n];
+        var rpT = new Date()
+        var n = 0;
+        time_is_widget.timeDiff = rpT.getTime() - t - Math.round((rpT - r) / 2);
+        console.log("cb config", this.config);
+        for (i in this.config) {
+            this.config[i]["vector"] = a[n];
             n++;
         }
         this.tick();
-    }
+    };
+
     this.tick = function() {
-        var tU = new Date(), t = new Date(), c, pw
-        tU.setTime(tU.getTime() - this.tD);
-        for (i in this.ca) {
-            c = this.ca[i];
-            if (typeof c["v"][0] != U) {
-                if ((0 < c["v"][1]) && (c["v"][1] < tU.getTime())) {
-                    c["v"][0] = c["v"][2];
-                    c["v"][1] = 0;
+        var tU = new Date(), t = new Date(), c, pw;
+        tU.setTime(tU.getTime() - this.timeDiff);
+        // this for loop is to allow for multiple times to be displayed based on how many locations you list in the config obj
+        for (i in this.config) {
+            c = this.config[i];
+            if (typeof c["vector"][0] != U) {
+                if ((0 < c["vector"][1]) && (c["vector"][1] < tU.getTime())) {
+                    c["vector"][0] = c["vector"][2];
+                    c["vector"][1] = 0;
                 }
-                t.setTime(c["v"][0] * 60000 + tU.getTime());
-                var d, y = t.getUTCFullYear() + "", m = t.getUTCMonth() + 1, N = new Date(y,0,1), o = N.getDay() - 1
+                t.setTime(c["vector"][0] * 60000 + tU.getTime());
+                var d;
+                var y = t.getUTCFullYear() + "";
+                var m = t.getUTCMonth() + 1
+                var N = new Date(y,0,1);
+                var o = N.getDay() - 1;
                 if (o == -1) {
                     o = 6;
                 }
-                var W = Math.floor((t - N + N.getTimezoneOffset() * 60000) / 604800000 + (o / 7))
-                  , dn = p["dy"] + " " + Math.floor((t - N + N.getTimezoneOffset() * 60000) / 86400000 + 1)
+                var W = Math.floor((t - N + N.getTimezoneOffset() * 60000) / 604800000 + (o / 7)),
+                    dn = p["dy"] + " " + Math.floor((t - N + N.getTimezoneOffset() * 60000) / 86400000 + 1);
                 if (o < 4) {
                     W++;
                 }
@@ -139,44 +156,43 @@ function time_is_widget() {
                 }
                 var g = {
                     t: t.getUTCHours(),
-                    r: c["v"][3],
-                    s: c["v"][5]
+                    r: c["vector"][3],
+                    s: c["vector"][5]
                 }
-                  , h = {}
+                  , h = {};
                 for (j in g) {
-                    h[j] = l0(g[j]);
+                    h[j] = addLeadingZero(g[j]);
                     h[j + "H"] = h[j];
                     h[j + "M"] = "AM";
                     if (11 < h[j]) {
                         h[j + "M"] = "PM";
-                        h[j] = l0(h[j] - 12);
+                        h[j] = addLeadingZero(h[j] - 12);
                         if (h[j] == "00") {
                             h[j] = 12;
                         }
                     }
                 }
-                // console.log('h', h);
                 d = c["template"]
                     .replace("srhour", h["rH"])
                     .replace("sr12hour", h["r"])
                     .replace("srAMPM", '<span id="ampm">' + h["rM"] + '</span>')
-                    .replace("srminute", l0(c["v"][4]))
+                    .replace("srminute", addLeadingZero(c["vector"][4]))
                     .replace("sshour", h["sH"])
                     .replace("ss12hour", h["s"])
                     .replace("ssAMPM", '<span id="ampm">' + h["sM"] + '</span>')
-                    .replace("ssminute", l0(c["v"][6]))
-                    .replace("dlhours", c["v"][7])
-                    .replace("dlminutes", c["v"][8])
+                    .replace("ssminute", addLeadingZero(c["vector"][6]))
+                    .replace("dlhours", c["vector"][7])
+                    .replace("dlminutes", c["vector"][8])
                     .replace("%h", h["t"])
                     .replace("%H", h["tH"])
-                    .replace("%i", l0(t.getUTCMinutes()))
-                    .replace("%s", l0(t.getUTCSeconds()))
+                    .replace("%i", addLeadingZero(t.getUTCMinutes()))
+                    .replace("%s", addLeadingZero(t.getUTCSeconds()))
                     .replace("%A", '<span id="ampm">' + h["tM"] + '</span>')
                     .replace("%j", t.getUTCDate())
-                    .replace("%d", l0(t.getUTCDate()))
+                    .replace("%d", addLeadingZero(t.getUTCDate()))
                     .replace("%W", pw)
                     .replace("%n", m)
-                    .replace("%m", l0(m))
+                    .replace("%m", addLeadingZero(m))
                     .replace("%y", y.substr(2, 2))
                     .replace("%Y", y)
                     .replace("%F", p["n"][13 + m])
@@ -185,12 +201,9 @@ function time_is_widget() {
                     .replace("%z", dn);
                 j = d.indexOf(">");
                 d = d.substr(0, j + 1) + d.substr(j + 1, 1).toUpperCase() + d.substr(j + 2, d.length - 1);
-                // console.log('d', d);
                 if (d != c["p"]) {
                     if (document.getElementById) {
                         o = document.getElementById(i);
-                        // console.log('i', i);
-                        // console.log('o', o);
                     }
                     else {
                         o = eval();
@@ -205,9 +218,10 @@ function time_is_widget() {
                 eval(c["callback"] + "(d)");
             }
         }
-        tout = setTimeout('time_is_widget.tick("")', updint - tU % updint);
+        timeout = setTimeout('time_is_widget.tick("")', updint - tU % updint);
     }
-    function l0(n) {
+
+    function addLeadingZero(n) {
         return n > 9 ? n : "0" + n;
     }
 }
