d484c1222829993ea0cbf3ced4b1c2f131389ebf
Samuel Kwong <samvictor232@gmail.com>
2018-06-23 16:10:48 -0700

    Changed url naming. Template loading problem still exists.

--- ap/semiannual/urls.py
+++ ap/semiannual/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from semiannual import views
 urlpatterns = [
   url(r'^rolls/$', views.SemiAnnualView.as_view(), name='semi-rolls'),
-  url(r'^report/$', views.SemiAnnualStudyReport.as_view(), name='semi-study-report'),
+  url(r'^rolls/report/$', views.SemiAnnualStudyReport.as_view(), name='semi-study-report'),
 ] 
