05e07051b91fc46ddace4e8e62cb535cd525b257
iamsitting <cdsalamanca27@gmail.com>
2018-10-11 20:49:35 -0700

    admin for answers

--- ap/gospel_trips/admin.py
+++ ap/gospel_trips/admin.py
@@ -6,9 +6,17 @@ from django.contrib import admin
 from .models import (Answer, AnswerChoice, GospelTrip, NonTrainee, Question,
                      Section)
 
+
+class AnswerAdmin(admin.ModelAdmin):
+  list_display = ('pk', 'trainee', 'question')
+  list_filter = ('gospel_trip', 'trainee', 'question')
+  ordering = ('gospel_trip', 'question')
+  search_fields = ('pk', 'trainee__firstname', 'trainee__lastname', 'gospel_trip__name', 'question__section__name')
+
+
 admin.site.register(GospelTrip)
 admin.site.register(Section)
 admin.site.register(Question)
-admin.site.register(Answer)
+admin.site.register(Answer, AnswerAdmin)
 admin.site.register(AnswerChoice)
 admin.site.register(NonTrainee)
