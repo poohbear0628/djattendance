2dfaa8bd234294da1c34529b93d42da2e783167b
zioninzion <zionpark93@gmail.com>
2018-10-20 15:49:42 -0700

    gospel-pair-working

--- ap/gospel_statistics/urls.py
+++ ap/gospel_statistics/urls.py
@@ -4,7 +4,7 @@ from gospel_statistics import views
 
 urlpatterns = [
   url(r'^$', views.GospelStatisticsView.as_view(), name='gospel-statistics-view'),
-  url(r'^new_pair$', views.NewGospelPairView.as_view(), name='new-pair'),
+  url(r'^new_pair/$', views.NewGospelPairView.as_view(), name='new-pair'),
   #Change following urls
   url(r'^ta$', views.TAGospelStatisticsView, name='ta-gospel-statistics-view'),
   url(r'^weekly_statistics$', views.weekly_statistics, name='weekly-statistics'),
--- ap/gospel_statistics/views.py
+++ ap/gospel_statistics/views.py
@@ -3,12 +3,13 @@ from __future__ import unicode_literals
 
 import json
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
 from django.views.generic.edit import DeleteView
 from django.views.generic import TemplateView
 from django.http import HttpResponse
 
-from .models import GospelStat
+from .models import GospelStat, GospelPair
 from terms.models import Term
 from accounts.models import Trainee
 
@@ -25,7 +26,8 @@ for i in _attributes:
 
 def get_week():
   for i in range(0,21):
-    if Term.current_term().startdate_of_week(i) <= date.today() and Term.current_term().enddate_of_week(i) >= date.today():
+    if Term.current_term().startdate_of_week(i) <= date.today()\
+    and Term.current_term().enddate_of_week(i) >= date.today():
       return i
 
 #In Progress
@@ -37,8 +39,7 @@ class GospelStatisticsView(TemplateView):
     context = super(GospelStatisticsView, self).get_context_data(**kwargs)
     context['page_title'] = 'Team Statistics'
     context['team'] = current_user.team
-    #delete
-    context['gospel_pairs'] = current_user.firstname+' '+current_user.lastname
+    context['gospel_pairs'] = GospelPair.objects.filter(team=current_user.team)
     context['cols'] = attributes
     context['week'] = get_week()
     return context
@@ -46,13 +47,29 @@ class GospelStatisticsView(TemplateView):
 class NewGospelPairView(TemplateView):
   template_name = "gospel_statistics/new_pair.html"
 
+  def post(self, request, *args, **kwargs):
+    context = self.get_context_data()
+    list_of_trainee_id = request.POST.getlist('inputs')
+    #Check for duplicate
+    list_of_trainees = []
+    for each in list_of_trainee_id:
+      list_of_trainees.extend(Trainee.objects.filter(id=each))
+    gospelpair = GospelPair(team=context['team'], term=Term.current_term())
+    gospelpair.save()
+    for each in list_of_trainees:
+      print each
+      gospelpair.trainees.add(each)
+    return redirect(reverse('gospel_statistics:gospel-statistics-view'))
+
   def get_context_data(self, **kwargs):
     current_user = self.request.user
     context = super(NewGospelPairView, self).get_context_data(**kwargs)
     context['page_title'] = 'New Gospel Pair'
     context['team'] = current_user.team
+    context['members'] = Trainee.objects.filter(team=current_user.team)
     return context
 
+#Delete
 def weekly_statistics(request):
   current_week = get_week()
   current_team = request.user.team
